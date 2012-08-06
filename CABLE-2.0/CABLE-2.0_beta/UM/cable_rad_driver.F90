023bdb38a992d8bf07d3dbab8ced2e6afa00b2f4
Jhan Srbinovsky <jxs599@nci.org.au>
2012-08-06 00:00:00 +0000

    merging Rachels branch - headers mostly - r 100

--- CABLE-2.0/CABLE-2.0_beta/UM/cable_rad_driver.F90
+++ CABLE-2.0/CABLE-2.0_beta/UM/cable_rad_driver.F90
@@ -1,3 +1,32 @@
+!==============================================================================
+! This source code is part of the 
+! Australian Community Atmosphere Biosphere Land Exchange (CABLE) model.
+! This work is licensed under the CABLE Academic User Licence Agreement 
+! (the "Licence").
+! You may not use this file except in compliance with the Licence.
+! A copy of the Licence and registration form can be obtained from 
+! http://www.accessimulator.org.au/cable
+! You need to register and read the Licence agreement before use.
+! Please contact cable_help@nf.nci.org.au for any questions on 
+! registration and the Licence.
+!
+! Unless required by applicable law or agreed to in writing, 
+! software distributed under the Licence is distributed on an "AS IS" BASIS,
+! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+! See the Licence for the specific language governing permissions and 
+! limitations under the Licence.
+! ==============================================================================
+!
+! Purpose: Pass CABLE albedo to UM variable for UM radiation scheme
+!
+! Called from: UM code glue_rad_ctl
+!
+! Contact: Jhan.Srbinovsky@csiro.au
+!
+! History: Developed for CABLE v1.8
+!
+!
+! ==============================================================================
 
 SUBROUTINE cable_rad_driver(                                                   &
                              ! IN atmospheric forcing
@@ -28,7 +57,7 @@ SUBROUTINE cable_rad_driver(                                                   &
       cos_zenith_angle
 
    REAL, DIMENSION(um1%row_length,um1%rows,4) ::                               &
-      LAND_ALBEDO_CABLE, & ! Land albedo calculated by Cable
+      LAND_ALBEDO_CABLE, & ! Land albedo calculated by Cable NIR/VIS/Beam/Diffuse
       surf_down_sw         ! IN Surface downward SW radiation
 
    REAL, DIMENSION(um1%LAND_PTS,um1%NTILES) ::                                 &
