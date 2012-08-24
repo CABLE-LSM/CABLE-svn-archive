4c156570931d0965e37e4a193b5db1afcf960d75
Bernard Pak <bep599@nci.org.au>
2012-08-24 07:16:50 +0000

    merged r159-163 to my branch

--- bep599/CABLE-2.0_beta/core/biogeophys/cable_cbm.F90
+++ bep599/CABLE-2.0_beta/core/biogeophys/cable_cbm.F90
@@ -138,18 +138,22 @@ CONTAINS
    ENDIF
 
    ssnow%deltss = ssnow%tss-ssnow%otss
+   ! correction required for energy balance in online simulations
+   IF( cable_runtime%um ) THEN
+   
+      canopy%fhs = canopy%fhs + ( ssnow%tss-ssnow%otss ) * ssnow%dfh_dtg
+      
+      canopy%fhs_cor = canopy%fhs_cor + ( ssnow%tss-ssnow%otss ) * ssnow%dfh_dtg
+      
+      canopy%fh = canopy%fhv + canopy%fhs
 
-   canopy%fhs = canopy%fhs + ( ssnow%tss-ssnow%otss ) * ssnow%dfh_dtg
-   
-   canopy%fhs_cor = canopy%fhs_cor + ( ssnow%tss-ssnow%otss ) * ssnow%dfh_dtg
-   
-   canopy%fh = canopy%fhv + canopy%fhs
+      canopy%fes = canopy%fes + ( ssnow%tss-ssnow%otss ) *                     &
+                   ( ssnow%cls * ssnow%dfe_ddq * ssnow%ddq_dtg )
+      
+      canopy%fes_cor = canopy%fes_cor + ( ssnow%tss-ssnow%otss ) *             &
+                       ( ssnow%cls * ssnow%dfe_ddq * ssnow%ddq_dtg )
 
-   canopy%fes = canopy%fes + ( ssnow%tss-ssnow%otss ) *                        &
-                ( ssnow%cls * ssnow%dfe_ddq * ssnow%ddq_dtg )
-   
-   canopy%fes_cor = canopy%fes_cor + ( ssnow%tss-ssnow%otss ) *                &
-                    ( ssnow%cls * ssnow%dfe_ddq * ssnow%ddq_dtg )
+   ENDIF
 
    ! need to adjust fe after soilsnow
    canopy%fev  = canopy%fevc + canopy%fevw
