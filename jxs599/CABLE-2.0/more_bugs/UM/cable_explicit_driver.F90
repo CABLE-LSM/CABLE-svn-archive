0fe60f8b3abc85dc4796f6401bd76081ac104fbb
Jhan Srbinovsky <jxs599@nci.org.au>
2012-09-20 05:32:04 +0000

    160 exported over the top. reverted all build.ksh stuff and offline things

--- jxs599/CABLE-2.0/more_bugs/UM/cable_explicit_driver.F90
+++ jxs599/CABLE-2.0/more_bugs/UM/cable_explicit_driver.F90
@@ -238,7 +238,10 @@ SUBROUTINE cable_explicit_driver( row_length, rows, land_pts, ntiles,npft,     &
    
    !___ location of namelist file defining runtime vars
    CHARACTER(LEN=200), PARAMETER ::                                            & 
-      runtime_vars_file = 'cable.nml'
+      nml_path1 = '/CABLE-AUX/UM/cable.nml'
+
+   CHARACTER(LEN=200) ::                                                       & 
+      runtime_vars_file 
 
 
    !___ 1st call in RUN (!=ktau_gl -see below) 
@@ -246,10 +249,15 @@ SUBROUTINE cable_explicit_driver( row_length, rows, land_pts, ntiles,npft,     &
  
 
 
+
    !--- initialize cable_runtime% switches 
    IF(first_cable_call)                                                        & 
       cable_runtime%um = .TRUE.
    
+   ! set namelist location in users home directory
+   CALL GETENV("HOME", myhome)   
+   runtime_vars_file = TRIM(myhome)//TRIM(nml_path1)
+
    !--- basic info from global model passed to cable_common_module 
    !--- vars so don't need to be passed around, just USE _module
    ktau_gl = timestep_number     !timestep of EXPERIMENT not necesarily 
