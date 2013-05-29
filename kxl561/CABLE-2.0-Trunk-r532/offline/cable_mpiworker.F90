de8349fc012aadd663af5a8d1537114251d65f7d
Kai Lu <kxl561@nci.org.au>
2013-05-29 00:54:35 +0000

    Adding receiving soil color from master

--- kxl561/CABLE-2.0-Trunk-r532/offline/cable_mpiworker.F90
+++ kxl561/CABLE-2.0-Trunk-r532/offline/cable_mpiworker.F90
@@ -108,7 +108,7 @@ CONTAINS
    USE cable_def_types_mod
    USE cable_IO_vars_module, ONLY: logn,gswpfile,ncciy,leaps,                  &
                                    verbose, fixedCO2,output,check,patchout,    &
-                                   patch_type,soilparmnew
+                                   patch_type,soilparmnew,calcsoilalbedo
    USE cable_common_module,  ONLY: ktau_gl, kend_gl, knode_gl, cable_user,     &
                                    cable_runtime, filename, myhome,            & 
                                    redistrb, wiltParam, satuParam
@@ -206,6 +206,7 @@ CONTAINS
                   filename,         & ! TYPE, containing input filenames 
                   vegparmnew,       & ! jhan: use new soil param. method
                   soilparmnew,      & ! jhan: use new soil param. method
+                  calcsoilalbedo,   & ! Newly added by Kai Requested by Jatin
                   spinup,           & ! spinup model (soil) to steady state 
                   delsoilM,delsoilT,& ! 
                   output,           &
@@ -1206,6 +1207,13 @@ SUBROUTINE worker_cable_params (comm,met,air,ssnow,veg,bgc,soil,canopy,&
   CALL MPI_Get_address (soil%zshh, displs(bidx), ierr)
   blen(bidx) = (ms + 1) * extr1
 
+  ! Newly added IF BLOCK by Kai
+  IF (calcsoilalbedo) THEN
+     bidx = bidx + 1
+     CALL MPI_Get_address (soil%soilcol, displs(bidx), ierr)
+     blen(bidx) = r1len
+  END IF
+
   ! ----------- canopy --------------
 
   bidx = bidx + 1
@@ -1958,6 +1966,10 @@ SUBROUTINE worker_casa_params (comm,casabiome,casapool,casaflux,casamet,&
   END IF
 
   ntyp = ncasaparam
+  ! Newly added IF BLOCK by Kai
+  IF (calcsoilalbedo) THEN
+     ntyp = 284
+  ENDIF
 
   ALLOCATE (blen(ntyp))
   ALLOCATE (displs(ntyp))
