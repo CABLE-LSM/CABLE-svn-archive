34c75b1b7297754f2ea0984202d05aee1eb3210c
Mark Decker <mrd561@nci.org.au>
2013-11-19 04:35:27 +0000

    initial commit of routing module skeleton.  Basing routing off of clm+other modles to run at a high resolution that is independent of the cable simulations

---
+++ mrd561/CABLE-2.1.2/core/riverrouting/cable_routing.f90
@@ -0,0 +1,32 @@
+module cable_routing
+
+
+
+contains
+
+subroutine find_downstream_index()
+end subroutine find_downstream_index
+
+
+subroutine find_stream_length()
+end subroutine find_stream_length
+
+subroutine map_cable2river()
+end subroutine map_cable2river
+
+subroutine step_riverflow()
+end subroutine step_riverflow
+
+subroutine map_river2cable()
+end subroutine map_river2cable
+
+subroutine mass_balance()
+end surbourinte mass_balance
+
+
+
+
+
+
+
+end module cable_routing
