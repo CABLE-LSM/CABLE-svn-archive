564c9876e33688d6524f2433767945ce7de1c03d
Bernard Pak <bep599@nci.org.au>
2014-02-13 02:08:28 +0000

    Merged bug fix from Tilo's branch

--- bep599/CABLE-2.0_Tilo/core/biogeochem/casa_cnp.F90
+++ bep599/CABLE-2.0_Tilo/core/biogeochem/casa_cnp.F90
@@ -1114,7 +1114,7 @@ IF(casamet%iveg2(nland)/=icewater) THEN
 !                       *  max(0.0,(casapool%Psoil(nland,2)+casapool%Psoil(nland,3))) &
 !                       *  max(0.0,(costNpup(veg%iveg(nland))-15.0))/(max(0.0,(costNpup(veg%iveg(nland))-15.0)) + 150.0)
 
-      fluxptase(nland) =  prodptase(veg%iveg(nland))  &
+      fluxptase(nland) =  prodptase(veg%iveg(nland))*deltcasa  &
                        *  max(0.0,(casapool%Psoil(nland,2)*casaflux%ksoil(nland,2)+casapool%Psoil(nland,3)*casaflux%ksoil(nland,3))) &
                        *  max(0.0,(costNpup(veg%iveg(nland))-15.0))/(max(0.0,(costNpup(veg%iveg(nland))-15.0)) + 150.0)
 
