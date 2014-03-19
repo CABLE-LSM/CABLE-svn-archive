88406f2d6406873f10dab7eb62e8fc6b9a4ad713
Jhan Srbinovsky <jxs599@nci.org.au>
2014-03-19 03:36:53 +0000

    rm these directories. supercededby March 2014 branch

--- JULES/prototype/jhan/2013work/JULES/JULES-3.1ish/includes/shared/c_epslon.h
+++
@@ -1,7 +0,0 @@
-!*L------------------COMDECK C_EPSLON-----------------------------------
-! EPSILON IS RATIO OF MOLECULAR WEIGHTS OF WATER AND DRY AIR
-
-      Real, Parameter :: Epsilon   = 0.62198
-      Real, Parameter :: C_Virtual = 1./Epsilon-1.
-
-!*----------------------------------------------------------------------
--- JULES/prototype/jhan/2013work/JULES/JULES-3.1ish/includes/shared/c_vkman.h
+++
@@ -1,3 +0,0 @@
-! C_VKMAN start
-      REAL,PARAMETER:: VKMAN=0.4 ! Von Karman's constant
-! C_VKMAN end
