5eaa8153e41de00743af9cd9e2732c42ab75aee6
Martin De Kauwe <mgk576@nci.org.au>
2014-06-03 01:24:57 +0000

    Removed 1.6 from gs eqn

--- jtk561/CABLEv2.0-trunk-Medlyn-Stom-Resis/core/biogeophys/cable_canopy.F90
+++ jtk561/CABLEv2.0-trunk-Medlyn-Stom-Resis/core/biogeophys/cable_canopy.F90
@@ -1533,17 +1533,17 @@ SUBROUTINE dryLeaf( dels, rad, rough, air, met,                                &
                 ELSE
                     vpd = dsx(i) * 1E-03 ! Pa -> kPa  
                 END IF   
-               
+                
                 g1 = (veg%g1c3(i) * (1.0 - veg%frac4(i))) + &
                      (veg%g1c4(1)  * veg%frac4(i))
-
-                gs_coeff(i,1) = (1.0 + 1.6 * (g1 * fwsoil(i)) / &
-                                 SQRT(vpd)) / (csx(i,1)*1E6) ! convert CO2 bar to umol/mol
-
-                gs_coeff(i,2) = (1.0 + 1.6 * (g1 * fwsoil(i)) / &
-                                 SQRT(vpd)) / (csx(i,2)*1E6) ! convert CO2 bar to umol/mol
-
-
+            
+                gs_coeff(i,1) = (1.0 + (g1 * fwsoil(i) / SQRT(vpd))) / &
+                                (csx(i,1)*1E6) ! convert CO2 bar to umol/mol
+                                           
+                gs_coeff(i,2) = (1.0 + (g1 * fwsoil(i) / SQRT(vpd))) / &
+                                (csx(i,2)*1E6) ! convert CO2 bar to umol/mol
+            
+            
                 ! Convert gs from mol to umol
                 gs_coeff(i,1) = gs_coeff(i,1) * 1E6
                 gs_coeff(i,2) = gs_coeff(i,2) * 1E6
