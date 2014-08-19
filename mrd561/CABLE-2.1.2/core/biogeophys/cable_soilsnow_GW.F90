ceac8fb74d91839c91707be4acee60c001cce215
Mark Decker <mrd561@nci.org.au>
2014-08-19 21:26:42 +0000

    wetfrac = top + (1-top)*x*x

--- mrd561/CABLE-2.1.2/core/biogeophys/cable_soilsnow_GW.F90
+++ mrd561/CABLE-2.1.2/core/biogeophys/cable_soilsnow_GW.F90
@@ -2251,7 +2251,7 @@ SUBROUTINE calc_srf_wet_fraction(ssnow,soil)
 
     xx(:) = max(1.e-3, min(1., real((ssnow%wbliq(:,1)-0.5*soil%swilt(:))/&
                                     (soil%sfc(:)-0.5*soil%swilt(:)))))
-    !xx(:) = max(1.e-6, min(1., xx*xx))
+    xx(:) = max(1.e-6, min(1., xx*xx))
 
     xxx(:) = xx!(exp(-3.0*xx(:))-1.) / (exp(-3.0)-1.0)
 
