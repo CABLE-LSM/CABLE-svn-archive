b34cac22443f5f909c6fdd260d6929062f967447
Mark Decker <mrd561@nci.org.au>
2014-08-20 02:12:05 +0000

    testing wetfrac = top + (1-top)*(exp(-x)-1)/(exp(-1)-1)

--- mrd561/CABLE-2.1.2/core/biogeophys/cable_soilsnow_GW.F90
+++ mrd561/CABLE-2.1.2/core/biogeophys/cable_soilsnow_GW.F90
@@ -2253,7 +2253,7 @@ SUBROUTINE calc_srf_wet_fraction(ssnow,soil)
                                     (soil%sfc(:)-0.5*soil%swilt(:)))))
     !xx(:) = max(1.e-6, min(1., xx*xx))
 
-    xxx(:) = (exp(-2.0*xx(:))-1.) / (exp(-2.0)-1.0)
+    xxx(:) = (exp(-1.0*xx(:))-1.) / (exp(-1.0)-1.0)
 
     satfrac(:) = (1._r_2-fice(:))*gw_params%MaxSatFraction*exp(-wtd_meters/gw_params%EfoldMaxSatFrac)+fice(:)
 
