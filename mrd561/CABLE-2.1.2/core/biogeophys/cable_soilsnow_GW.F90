9cd95f107a4b65850a766636b6930c95ca1ecefc
Mark Decker <mrd561@nci.org.au>
2014-08-23 22:53:14 +0000

    use 0.25 for swilt.  exp(-2)-1 for wet frac

--- mrd561/CABLE-2.1.2/core/biogeophys/cable_soilsnow_GW.F90
+++ mrd561/CABLE-2.1.2/core/biogeophys/cable_soilsnow_GW.F90
@@ -2168,11 +2168,11 @@ SUBROUTINE calc_srf_wet_fraction(ssnow,soil)
     wtd_meters = ssnow%wtd / 1000._r_2
 
 
-    xx(:) = max(1.e-6, min(1., real((ssnow%wbliq(:,1)-0.5*soil%swilt(:))/&
-                                    (soil%sfc(:)-0.5*soil%swilt(:)))))
+    xx(:) = max(1.e-6, min(1., real((ssnow%wbliq(:,1)-0.25*soil%swilt(:))/&
+                                    (soil%sfc(:)-0.25*soil%swilt(:)))))
     !xx(:) = max(1.e-6, min(1., xx*xx))
 
-    !xxx(:) = (exp(-1.0*xx(:))-1.) / (exp(-1.0)-1.0)
+    xxx(:) = (exp(-2.0*xx(:))-1.) / (exp(-2.0)-1.0)
 
     satfrac(:) = (1._r_2-fice(:))*gw_params%MaxSatFraction*exp(-wtd_meters/gw_params%EfoldMaxSatFrac)+fice(:)
 
