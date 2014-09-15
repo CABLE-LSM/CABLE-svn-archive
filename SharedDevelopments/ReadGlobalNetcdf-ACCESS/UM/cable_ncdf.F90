86d79cf9b4e7140d8e3b1444d0a58ef6f561e661
Jhan Srbinovsky <jxs599@nci.org.au>
2014-09-15 23:49:19 +0000

    effectively restore these file s back to 2201

--- SharedDevelopments/ReadGlobalNetcdf-ACCESS/UM/cable_ncdf.F90
+++ SharedDevelopments/ReadGlobalNetcdf-ACCESS/UM/cable_ncdf.F90
@@ -1,5 +1,5 @@
-! module cable_ncdf_module
-! contains
+!module cable_ncdf_module
+!contains
 subroutine predef_grid(latitude, longitude, node_gl, rows, row_length, mp,       &
                      npseudo,mydata)
    use netcdf
@@ -7,7 +7,7 @@ subroutine predef_grid(latitude, longitude, node_gl, rows, row_length, mp,      
  
    ! we are passing these as they are declared i8 and r8
    integer :: node_gl, rows, row_length, mp, npseudo
-   REAL(kind=8), dimension(mp) :: latitude, longitude 
+   REAL, dimension(mp) :: latitude, longitude 
    real, dimension(mp, npseudo) :: mydata 
 
    ! This is the name of the data file we will read. 
@@ -84,16 +84,7 @@ subroutine predef_grid(latitude, longitude, node_gl, rows, row_length, mp,      
    call check( nf90_get_var(ncid, varid, data_in) )
    !call check( nf90_get_var(ncid, varid, mydata, & 
    !            start=(/latitude(1),longitude(1),1/),count=(/rows,row_length,12/) ) )
-   
-    print *,"after reading in predef_grid:"
-    do x=1, 12
-     print *,sum(data_in(:,:,x),data_in(:,:,x)<100)/max(1,count(data_in(:,:,x)<100))
-    end do
-
-   print *,"cable_UM_latitude",latitude
-   ! print *,"*********************************latitude checking************************************"
-    print *, "read in lat",lat_in(:,1)
-  
+ 
    ! Check the data.
    !lon, lat, t
    !row_length, rows, t
@@ -102,16 +93,16 @@ subroutine predef_grid(latitude, longitude, node_gl, rows, row_length, mp,      
    do x = 1, nlon
       do y = 1, nlat
          do i = 1, mp
-            if( (ABS(lon_in(x,1) - longitude(i)) < 0.001) & 
-            .AND. ABS(lat_in(y,1) - latitude(i))< 0.001) then 
+            if( lon_in(x,1) == longitude(i) & 
+            .AND. lat_in(y,1) == latitude(i) ) then 
                !print *, "z,y,x,i ", z, y, x, i
                mydata(i,z) =   data_in(x, y, z)   
-                print *, "data_in  ", data_in(x, y, z) 
+               !print *, "data_in  ", data_in(x, y, z) 
                !print *, "mydata ", mydata(i,z)
                x_in(i) = x
-               ! print *, "x_in ", x_in(i)
+               !print *, "x_in ", x_in(i)
                y_in(i) = y
-               ! print *, "y_in ", y_in(i)
+               !print *, "y_in ", y_in(i)
                if( i > mp) print *, "Count has reached mp"
             endif
          end do
@@ -141,6 +132,151 @@ subroutine predef_grid(latitude, longitude, node_gl, rows, row_length, mp,      
 
 end subroutine predef_grid
 
-! end module cable_ncdf_module
+!end module cable_ncdf_module
+!program sum                                          !a: name of program
+!  implicit none
+!  
+!    ! temporary variables
+!    integer, parameter :: file_id = 15,file_daily_id=20,file_shift_daily_id =25
+!    character(len=100) :: junk_head,label, input_file, temp_shift_days
+!    
+!    integer  :: shift_days = 20
+!    integer, parameter :: days_of_year=365
+!    integer, parameter :: months_of_year=12
+!    integer :: i_day
+!
+!    ! define arrary for reading the monthly lai data
+!    real :: monthly_lai_array(2,12)  ! colomum major function pay attehtion
+!    
+!    ! variables lai data before and after interpolation and shift 
+!    real :: monthly_lai(months_of_year)
+!    real :: daily_lai(days_of_year)
+!    
+!    !initialize the daily lai array
+!    DATA daily_lai /days_of_year*-9999/
+!    
+!    ! switch whether the using default lai input and shifting days or input as argument
+!    IF (command_argument_count() >1) THEN
+!      print *, "using the argument variables"
+!      call get_command_argument(1,input_file)
+!      call get_command_argument(2,temp_shift_days)
+!      print *, "argument one ****   ", input_file
+!      print *, "argument two *** ",    temp_shift_days
+!      ! convert the string into iterger
+!      open(file_id,file = input_file)
+!      read(temp_shift_days,'(I2)') shift_days
+!    ELSE ! using the defualt input file 
+!      open(file_id, file='north_lai.txt')  ! it will the lai from the north part of earth
+!    END IF
+!
+!   ! read the monhtly lai data
+!   ! read the junk head and print
+!     read(file_id,'(A)'), junk_head
+!     print *,'The head of the file is :', junk_head
+!   ! read the data into monthly lai arrary
+!     read(file_id,*),monthly_lai_array
+!
+!   ! extract monthly lai for interpolation 
+!     monthly_lai(:) = monthly_lai_array(2,:)
+!     close(file_id)  ! do not forget the close the file
+!  
+!    
+!   ! montly lai data is ready, it is time interplocation into daily lai
+!   ! write(*,6),monthly_lai
+!   print *, "interpolate the lai and write out"    
+!   call interp_linear_lai(monthly_lai,daily_lai) ! call interpolation function 
+!   ! write out the interpolated daily out
+!   open(file_daily_id,file='daily_lai.txt')
+!   do i_day=1, days_of_year
+!       write(file_daily_id,*) daily_lai(i_day)
+!   end do
+
+!   print *, "shifting the daily lai defined shifted days"
+   
+!   call advanced_shift_daily_lai(shift_days,daily_lai,monthly_lai) 
+   ! write out the shifted daily lai for comparison
+!   open(file_shift_daily_id,file="advanced_shift_daily_lai.txt")
+!    
+!   do i_day=1, days_of_year
+!       write(file_shift_daily_id,*) daily_lai(i_day)
+!   end do 
+!
+!   print *, "successfully interpolate and shifted the lai data"
+!   ! write(*,6),daily_lai
+!  
+!5   format(f16.4)
+!6   format(f16.4)
+!
+!end  program sum                                     !h: end of program 
  
+! subroutine for the interpolate the monthly lai data into daily lai
+  
+subroutine interp_linear_lai(monthly_lai,daily_lai)
+    implicit none
+    integer,parameter :: days_of_year=365
+    integer,parameter :: months_of_year=12
+    real,dimension(months_of_year) :: monthly_lai
+    real,dimension(days_of_year) :: daily_lai
+    integer :: month_date(months_of_year) = (/ 15,46,75,106,136,167,197,228,259,289,320,350 /) 
+    
+    integer :: i_day, i_month, first_date_id, second_date_id, delta_days
+    real  :: first_date_lai, second_date_lai
+    real  :: delta_lai
+    
+    ! interpolation start
+    do i_month=1, months_of_year-1  ! do not go to last month 
+       first_date_id   = month_date(i_month)
+       second_date_id  = month_date(i_month+1)
+       first_date_lai  = monthly_lai(i_month)
+       second_date_lai = monthly_lai(i_month+1)
+       delta_days      = second_date_id -first_date_id
+       delta_lai       = (second_date_lai-first_date_lai)/delta_days
+       
+       do i_day=1, delta_days+1     ! start date loop
+          daily_lai(first_date_id+i_day-1) = first_date_lai + (i_day-1)*delta_lai
+       end do ! end with the date loop
+       
+    end do ! end with month loop 
+    ! deal with special case day 1 to 15 and 350 to 365
+    daily_lai(1:14)    =  monthly_lai(1) ! 
+    daily_lai(351:365) =  monthly_lai(12)!
+
+
+    ! end with interpolation
+    return
+end subroutine interp_linear_lai   
+
+! define the shift subrountine
+subroutine advanced_shift_daily_lai(shift_days,daily_lai,monthly_lai)
+    integer :: shift_days
+    integer,parameter :: days_of_year=365
+    integer,parameter :: months_of_year=12
+    real :: daily_lai(days_of_year), monthly_lai(months_of_year)
+
+    integer :: pos_max(1),pos_min(1)
+    real :: diff_monthly_lai(months_of_year-1)
+    
+    ! calculate the difference of the monthly LAI in order to provide condition for shift condition
+    diff_monthly_lai = monthly_lai(2:months_of_year)-monthly_lai(1:(months_of_year-1))
+    diff_monthly_lai = abs(diff_monthly_lai) 
+
+    ! locate the position of the max and min lai vaule 
+    pos_max = MAXLOC(daily_lai)
+    pos_min = MINLOC(daily_lai) 
+
+    IF(maxval(diff_monthly_lai) > 1.3) THEN 
+      print *, "strange value, no shift"
+    ELSE ! normal case shift the lai 
+       IF(pos_max(1) > 90 .AND. pos_max(1) < 300)  THEN  ! north part   
+         daily_lai(1:(pos_max(1)-shift_days))              = daily_lai((shift_days+1):pos_max(1))
+
+         daily_lai( (pos_max(1)-shift_days+1):pos_max(1)) = daily_lai(pos_max(1)) ! repeat the max value shift times
+       ELSE 
+         daily_lai(pos_min(1):(days_of_year-shift_days))   = daily_lai((pos_min(1)+shift_days):days_of_year)  !(south part)
+
+         daily_lai( (days_of_year-shift_days+1):days_of_year) = daily_lai(days_of_year)        
+        END IF ! end with north south loop
+    END IF ! end with normal if
+     
+end subroutine advanced_shift_daily_lai
 
