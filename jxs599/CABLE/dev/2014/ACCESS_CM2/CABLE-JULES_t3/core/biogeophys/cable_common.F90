08c2d8f47c92a185df2ea30ceeccb238d0236321
Jhan Srbinovsky <jxs599@nci.org.au>
2014-11-14 01:54:43 +0000

    subroutines in here added as is to FUDGE output from CABLE - debugging

--- jxs599/CABLE/dev/2014/ACCESS_CM2/CABLE-JULES_t3/core/biogeophys/cable_common.F90
+++ jxs599/CABLE/dev/2014/ACCESS_CM2/CABLE-JULES_t3/core/biogeophys/cable_common.F90
@@ -37,6 +37,7 @@ MODULE cable_common_module
    !---total number of timesteps, and processing node 
    INTEGER, SAVE :: ktau_gl, kend_gl, knode_gl, kwidth_gl
    
+   logical :: L_fudge = .false. 
    !---CABLE runtime switches def in this type
    TYPE kbl_internal_switches
       LOGICAL :: um = .FALSE., um_explicit = .FALSE., um_implicit = .FALSE.,   &
@@ -173,6 +174,10 @@ MODULE cable_common_module
 !jhan:temporary measure. improve hiding
 !   real, dimension(:,:), pointer,save :: c1, rhoch
       
+interface fudge_out
+   module procedure fudge_out_r2D, fudge_out_r1D, fudge_out_r3D, fudge_out_i2D
+End interface fudge_out
+
 CONTAINS
 
 
@@ -424,6 +429,83 @@ SUBROUTINE report_version_no( logn )
    CLOSE(440)
 
 END SUBROUTINE report_version_no
+
+SUBROUTINE fudge_out_i2D( i,j, var, varname, vzero, vval )
+   ! interfaces on these
+   integer :: i,j
+   integer, dimension(:,:) :: var
+   ! ft changes with interface
+   character(len=*), parameter :: &
+      ft = '(  "fudge: ", A10, "(", I2.1, ",", I2.1, X, ") = ", I1.1 )'
+   
+   character(len=*) :: varname
+   logical :: vzero
+   integer :: vval
+   
+   ! content changes with interface
+   var = var(i,j) 
+   if( (vzero) ) var = vval
+   write (6, ft) varname,i, var(i,j)
+End SUBROUTINE fudge_out_i2D 
+
+
+SUBROUTINE fudge_out_r1D( i, var, varname, vzero, vval )
+   ! interfaces on these
+   integer :: i
+   real, dimension(:) :: var
+   ! ft changes with interface
+   character(len=*), parameter :: &
+      ft = '(  "fudge: ", A10, "(", I2.1, X, ") = ", F15.3 )'
+   
+   character(len=*) :: varname
+   logical :: vzero
+   real :: vval
+
+   ! content changes with interface
+   var = var(i) 
+   if( (vzero) ) var = vval
+   write (6, ft) varname,i, var(i)
+End SUBROUTINE fudge_out_r1D 
+
+SUBROUTINE fudge_out_r2D( i,j, var, varname, vzero, vval )
+   ! interfaces on these
+   integer :: i,j
+   real, dimension(:,:) :: var
+   ! ft changes with interface
+   character(len=*), parameter :: &
+      ft = '(  "fudge: ", A10, "(", I2.1, ",", I2.1, X, ") = ", F15.3 )'
+   
+   character(len=*) :: varname
+   logical :: vzero
+   real :: vval
+   
+   ! content changes with interface
+   var = var(i,j) 
+   if( (vzero) ) var = vval
+   write (6, ft) varname,i,j, var(i,j)
+End SUBROUTINE fudge_out_r2D 
+
+SUBROUTINE fudge_out_r3D( i,j,k, var, varname, vzero, vval )
+   ! interfaces on these
+   integer :: i,j,k
+   real, dimension(:,:,:) :: var
+   ! ft changes with interface
+   character(len=*), parameter :: &
+      ft = '(  "fudge: ", A10, "(",  I2.1, ",",I2.1, ",", I2.1, X, ") = ", F15.3 )'
+   
+   character(len=*) :: varname
+   logical :: vzero
+   real :: vval
+   
+   ! content changes with interface
+   var = var(i,j,k) 
+   if( (vzero) ) var = vval
+   write (6, ft) varname,i,j,k, var(i,j,k)
+End SUBROUTINE fudge_out_r3D 
+
+
+
+
 
 
 
