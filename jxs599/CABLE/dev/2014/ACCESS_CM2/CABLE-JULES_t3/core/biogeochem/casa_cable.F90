14f680c8eaa27cd85df56cc7511886e9f0c220f9
Jhan Srbinovsky <jxs599@nci.org.au>
2014-11-25 04:10:36 +0000

    accept r1164+ : HEAD merge as changes to core should be irrelevant anyway to 8.5

--- jxs599/CABLE/dev/2014/ACCESS_CM2/CABLE-JULES_t3/core/biogeochem/casa_cable.F90
+++ jxs599/CABLE/dev/2014/ACCESS_CM2/CABLE-JULES_t3/core/biogeochem/casa_cable.F90
@@ -5,7 +5,7 @@
 ! (the "Licence").
 ! You may not use this file except in compliance with the Licence.
 ! A copy of the Licence and registration form can be obtained from 
-! http://www.accessimulator.org.au/cable
+! http://www.cawcr.gov.au/projects/access/cable
 ! You need to register and read the Licence agreement before use.
 ! Please contact cable_help@nf.nci.org.au for any questions on 
 ! registration and the Licence.
@@ -35,7 +35,7 @@ SUBROUTINE bgcdriver(ktau,kstart,kend,dels,met,ssnow,canopy,veg,soil, &
                      spinConv, spinup, ktauday, idoy, dump_read, dump_write )
 
    USE cable_def_types_mod
-   !USE cable_common_module, only: cable_runtime
+   USE cable_common_module, only: cable_runtime
    USE casadimension
    USE casaparm
    USE casavariable
@@ -69,7 +69,7 @@ SUBROUTINE bgcdriver(ktau,kstart,kend,dels,met,ssnow,canopy,veg,soil, &
    if ( .NOT. dump_read ) then
    ! Lest 13may13: will require loop when prog resp are init nonzero
    ! need for mk3l ?
-   !IF( cable_runtime%offline .or. cable_runtime%mk3l ) THEN
+   IF( .NOT. cable_runtime%UM ) THEN
       if(ktau == kstart) then
          casamet%tairk  = 0.0
          casamet%tsoil  = 0.0
@@ -86,7 +86,7 @@ SUBROUTINE bgcdriver(ktau,kstart,kend,dels,met,ssnow,canopy,veg,soil, &
          ! casaflux%crmplant(:,leaf) = 0.0
          ! end changes (BP jul2010)
       ENDIF
-   !ENDIF
+   ENDIF
       IF(mod(ktau,ktauday)==1) THEN
          casamet%tairk = met%tk
          casamet%tsoil = ssnow%tgg
@@ -309,7 +309,8 @@ END SUBROUTINE bgcdriver
 !  data xnslope/0.64,0.71,0.70,0.67,0.42,0.40,0.45,0.50,0.28,1.00,1.00,1.00,1.00,0.23,1.00,1.00,1.00/
 !  data xnslope/0.64,0.71,0.70,0.60,0.42,0.40,0.40,0.50,0.28,1.00,1.00,1.00,1.00,0.23,1.00,1.00,1.00/
 ! Q.Zhang: test parameters 13/09/2011
-  data xnslope/1.00,1.00,2.00,1.00,1.00,1.00,1.00,1.00,0.34,1.00,1.00,1.00,1.00,1.00,1.00,1.00,1.00/
+! Modified further in ACCESS-1.4
+  data xnslope/0.80,1.00,2.00,1.00,1.00,1.00,0.50,1.00,0.34,1.00,1.00,1.00,1.00,1.00,1.00,1.00,1.00/
 
   integer np,ivt
   real, dimension(mp)  :: ncleafx,npleafx  ! local variables
--- jxs599/CABLE/dev/2014/ACCESS_CM2/CABLE-JULES_t3/core/biogeophys/cable_define_types.F90
+++ jxs599/CABLE/dev/2014/ACCESS_CM2/CABLE-JULES_t3/core/biogeophys/cable_define_types.F90
@@ -5,7 +5,7 @@
 ! (the "Licence").
 ! You may not use this file except in compliance with the Licence.
 ! A copy of the Licence and registration form can be obtained from 
-! http://www.accessimulator.org.au/cable
+! http://www.cawcr.gov.au/projects/access/cable
 ! You need to register and read the Licence agreement before use.
 ! Please contact cable_help@nf.nci.org.au for any questions on 
 ! registration and the Licence.
@@ -120,6 +120,8 @@ MODULE cable_def_types_mod
          swilt,   & ! vol H2O @ wilting
          zse,     & ! thickness of each soil layer (1=top) in m
          zshh,    & ! distance between consecutive layer midpoints (m)
+  		 ! vars intro for Ticket #27
+         soilcol, & ! keep color for all patches/tiles
          albsoilf   ! soil reflectance
      
       REAL(r_2), DIMENSION(:), POINTER ::                                      &
@@ -593,7 +595,8 @@ SUBROUTINE alloc_soil_parameter_type(var, mp)
    allocate( var% cnsd(mp) )  
    allocate( var% albsoil(mp, nrb) )  
    allocate( var% pwb_min(mp) )  
-   allocate( var% albsoilf(mp) )  
+   allocate( var% albsoilf(mp) ) 
+   allocate( var% soilcol(mp) )
 
 END SUBROUTINE alloc_soil_parameter_type
  
@@ -993,7 +996,8 @@ SUBROUTINE dealloc_soil_parameter_type(var)
    DEALLOCATE( var% albsoil )  
    DEALLOCATE( var% cnsd )  
    DEALLOCATE( var% pwb_min)  
-   DEALLOCATE( var% albsoilf )  
+   DEALLOCATE( var% albsoilf )
+   DEALLOCATE( var% soilcol )  
    
 END SUBROUTINE dealloc_soil_parameter_type
  
