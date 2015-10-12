ab4d5ee497c28e4c201035dae5248478f6dacc63
Martin De Kauwe <mgk576@nci.org.au>
2015-10-12 09:48:56 +0000

    Added De Kauwe et al. Biogeosciences drought changes to cable_cbm

--- mgk576/CABLE-2.0.1-Tagged-plus-Medlyn-Stom-Param-drought/core/biogeophys/cable_cbm.F90
+++ mgk576/CABLE-2.0.1-Tagged-plus-Medlyn-Stom-Param-drought/core/biogeophys/cable_cbm.F90
@@ -1,28 +1,28 @@
 !==============================================================================
-! This source code is part of the 
+! This source code is part of the
 ! Australian Community Atmosphere Biosphere Land Exchange (CABLE) model.
-! This work is licensed under the CABLE Academic User Licence Agreement 
+! This work is licensed under the CABLE Academic User Licence Agreement
 ! (the "Licence").
 ! You may not use this file except in compliance with the Licence.
-! A copy of the Licence and registration form can be obtained from 
+! A copy of the Licence and registration form can be obtained from
 ! http://www.accessimulator.org.au/cable
 ! You need to register and read the Licence agreement before use.
-! Please contact cable_help@nf.nci.org.au for any questions on 
+! Please contact cable_help@nf.nci.org.au for any questions on
 ! registration and the Licence.
 !
-! Unless required by applicable law or agreed to in writing, 
+! Unless required by applicable law or agreed to in writing,
 ! software distributed under the Licence is distributed on an "AS IS" BASIS,
 ! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-! See the Licence for the specific language governing permissions and 
+! See the Licence for the specific language governing permissions and
 ! limitations under the Licence.
 ! ==============================================================================
 !
-! Purpose: Calls CABLE routines including define_air, surface_albedo, 
+! Purpose: Calls CABLE routines including define_air, surface_albedo,
 !          define_canopy, soilsnow, carbon
 !          Note that cbm is called once per timestep in the offline case but
-!          twice per timestep in the ACCESS case. Not all parts of cbm 
+!          twice per timestep in the ACCESS case. Not all parts of cbm
 !          are executed in each of the ACCESS calls.
-!          
+!
 ! Called from: cable_driver for offline version
 !              cable_explicit_driver, cable_implicit_driver for ACCESS
 !
@@ -36,21 +36,21 @@
 #define NO_CASA_YET 1
 
 MODULE cable_cbm_module
-   
+
    USE cable_canopy_module
    USE cable_albedo_module
-  
+
    IMPLICIT NONE
-  
+
    PRIVATE
-   PUBLIC cbm 
+   PUBLIC cbm
 
 CONTAINS
 
    SUBROUTINE cbm( dels, air, bgc, canopy, met,                                &
                    bal, rad, rough, soil,                                      &
                    ssnow, sum_flux, veg )
-    
+
    USE cable_common_module
    USE cable_carbon_module
    USE cable_soil_snow_module
@@ -61,9 +61,9 @@ CONTAINS
 #ifndef NO_CASA_YET
    USE casadimension,     only : icycle ! used in casa_cnp
 #endif
-   USE cable_data_module, ONLY : icbm_type, point2constants 
-   
-   !ptrs to local constants 
+   USE cable_data_module, ONLY : icbm_type, point2constants
+
+   !ptrs to local constants
    TYPE( icbm_type ) :: C
    ! CABLE model variables
    TYPE (air_type),       INTENT(INOUT) :: air
@@ -75,13 +75,13 @@ CONTAINS
    TYPE (roughness_type), INTENT(INOUT) :: rough
    TYPE (soil_snow_type), INTENT(INOUT) :: ssnow
    TYPE (sum_flux_type),  INTENT(INOUT) :: sum_flux
-    
-   TYPE (soil_parameter_type), INTENT(INOUT)   :: soil 
-   TYPE (veg_parameter_type),  INTENT(INOUT)    :: veg  
+
+   TYPE (soil_parameter_type), INTENT(INOUT)   :: soil
+   TYPE (veg_parameter_type),  INTENT(INOUT)    :: veg
 
    REAL, INTENT(IN)               :: dels ! time setp size (s)
-    
-   INTEGER :: k,kk,j  
+
+   INTEGER :: k,kk,j
 
 #ifdef NO_CASA_YET
    INTEGER :: ICYCLE
@@ -89,19 +89,19 @@ CONTAINS
 #endif
 
    ! assign local ptrs to constants defined in cable_data_module
-   CALL point2constants(C)    
+   CALL point2constants(C)
 
    IF( cable_runtime%um ) THEN
-      
+
       cable_runtime%um_radiation = .FALSE.
-      
+
       IF( cable_runtime%um_explicit ) THEN
          CALL ruff_resist(veg, rough, ssnow, canopy)
          met%tk = met%tk + C%grav/C%capp*(rough%zref_tq + 0.9*rough%z0m)
       ENDIF
-      
+
       CALL define_air (met, air)
-   
+
    ELSE
       call ruff_resist(veg, rough, ssnow, canopy)
    ENDIF
@@ -110,17 +110,19 @@ CONTAINS
    CALL init_radiation(met,rad,veg, canopy) ! need to be called at every dt
 
    IF( cable_runtime%um ) THEN
-      
+
       IF( cable_runtime%um_explicit ) THEN
          CALL surface_albedo(ssnow, veg, met, rad, soil, canopy)
       ENDIF
-   
+
    ELSE
       CALL surface_albedo(ssnow, veg, met, rad, soil, canopy)
    ENDIf
-    
+
    ! Calculate canopy variables:
-   CALL define_canopy(bal,rad,rough,air,met,dels,ssnow,soil,veg, canopy)
+   ! MDK 26 March 2015.
+   ! Added bgc as we need to pass root biomass to the new drought routinue
+   CALL define_canopy(bal,rad,rough,air,met,dels,ssnow,soil,veg, canopy, bgc)
 
    ssnow%otss_0 = ssnow%otss
    ssnow%otss = ssnow%tss
@@ -128,7 +130,7 @@ CONTAINS
    ssnow%owetfac = ssnow%wetfac
 
    IF( cable_runtime%um ) THEN
-      
+
      IF( cable_runtime%um_implicit ) THEN
          CALL soil_snow(dels, soil, ssnow, canopy, met, bal,veg)
       ENDIF
@@ -140,16 +142,16 @@ CONTAINS
    ssnow%deltss = ssnow%tss-ssnow%otss
    ! correction required for energy balance in online simulations
    IF( cable_runtime%um ) THEN
-   
+
       canopy%fhs = canopy%fhs + ( ssnow%tss-ssnow%otss ) * ssnow%dfh_dtg
-      
+
       canopy%fhs_cor = canopy%fhs_cor + ( ssnow%tss-ssnow%otss ) * ssnow%dfh_dtg
-      
+
       canopy%fh = canopy%fhv + canopy%fhs
 
       canopy%fes = canopy%fes + ( ssnow%tss-ssnow%otss ) *                     &
                    ( ssnow%cls * ssnow%dfe_ddq * ssnow%ddq_dtg )
-      
+
       canopy%fes_cor = canopy%fes_cor + ( ssnow%tss-ssnow%otss ) *             &
                        ( ssnow%cls * ssnow%dfe_ddq * ssnow%ddq_dtg )
 
@@ -157,7 +159,7 @@ CONTAINS
 
    ! need to adjust fe after soilsnow
    canopy%fev  = canopy%fevc + canopy%fevw
-  
+
    ! Calculate total latent heat flux:
    canopy%fe = canopy%fev + canopy%fes
 
@@ -168,7 +170,7 @@ CONTAINS
    rad%trad = ( ( 1.-rad%transd ) * canopy%tv**4 +                             &
               rad%transd * ssnow%tss**4 )**0.25
 
-   ! rml 17/1/11 move all plant resp and soil resp calculations here            
+   ! rml 17/1/11 move all plant resp and soil resp calculations here
    ! from canopy. in UM only call on implicit step.
    ! put old and new soil resp calculations into soilcarb subroutine
    ! make new plantcarb subroutine
@@ -176,7 +178,7 @@ CONTAINS
 
       !calculate canopy%frp
       CALL plantcarb(veg,bgc,met,canopy)
-     
+
       !calculate canopy%frs
       CALL soilcarb(soil, ssnow, veg, bgc, met, canopy)
 
@@ -187,9 +189,7 @@ CONTAINS
 
    ENDIF
 
-  
+
 END SUBROUTINE cbm
 
 END MODULE cable_cbm_module
-
-
