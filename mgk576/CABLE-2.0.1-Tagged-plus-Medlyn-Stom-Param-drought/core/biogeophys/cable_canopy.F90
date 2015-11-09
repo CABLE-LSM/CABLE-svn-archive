2bebddadc54e6bc782cac56ea916120da3d51135
Martin De Kauwe <mgk576@nci.org.au>
2015-11-09 01:15:55 +0000

    added print hod

--- mgk576/CABLE-2.0.1-Tagged-plus-Medlyn-Stom-Param-drought/core/biogeophys/cable_canopy.F90
+++ mgk576/CABLE-2.0.1-Tagged-plus-Medlyn-Stom-Param-drought/core/biogeophys/cable_canopy.F90
@@ -1665,7 +1665,7 @@ SUBROUTINE dryLeaf( dels, rad, rough, air, met,                                &
             !   !continue
             !   !print *, "Turned off recalc of transpiration"
 
-            print*, met%hod(i)
+            p
 
             ! Re-calculation of transpiration, to match new drought scheme
             ! Martin De Kauwe, 30th October 2015,
@@ -2220,6 +2220,7 @@ SUBROUTINE vcmax_non_stomatal_lim(fwsoil, fwsoil_ns, soil, ssnow, veg, i, bgc)
    REAL, DIMENSION(mp) :: Lsoil
    REAL, PARAMETER :: min_lwp = -2.0
 
+   print*, met%hod(i)
    ! Soil matric potential at saturation (m of head to MPA -> 9.81 * KPA_2_MPA)
    psi_sat_mpa = soil%sucs * 9.81 * 0.001
 
