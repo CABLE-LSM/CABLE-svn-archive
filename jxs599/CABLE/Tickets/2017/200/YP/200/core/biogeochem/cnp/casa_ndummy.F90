e6b924c3ab5bd4cc5dac22cf12af31315cff1df9
Jhan Srbinovsky <jxs599@nci.org.au>
2017-03-25 09:54:08 +0000

    merged

--- jxs599/CABLE/Tickets/2017/200/YP/200/core/biogeochem/cnp/casa_ndummy.F90
+++ jxs599/CABLE/Tickets/2017/200/YP/200/core/biogeochem/cnp/casa_ndummy.F90
@@ -1,18 +1,3 @@
-MODULE casa__mod
-
-USE cable_def_types_mod
-USE casadimension
-USE casaparm
-USE casavariable
-USE phenvariable
-USE cable_common_module, only: cable_user ! Custom soil respiration: Ticket #42
-
-IMPLICIT NONE
-  REAL(r_2), PARAMETER :: zero = 0.0_r_2
-  REAL(r_2), PARAMETER :: one  = 1.0_r_2
-
-CONTAINS
-
 SUBROUTINE casa_ndummy(casapool)
   IMPLICIT NONE
   TYPE (casa_pool),             INTENT(INOUT) :: casapool
@@ -21,4 +6,4 @@ SUBROUTINE casa_ndummy(casapool)
 
 END SUBROUTINE casa_ndummy
 
-END MODULE casa__mod
+
