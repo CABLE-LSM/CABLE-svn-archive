88964d0c603796677d9f1c8eb70dd57791b37646
Jhan Srbinovsky <jxs599@nci.org.au>
2017-08-28 07:16:52 +0000

    ''Quasi-CMIP6 trunk update''
    '''Type:''' Trivial - see https://trac.nci.org.au/trac/cable/wiki/PrioritiesACCESS-CM2
    
    '''Ticket:''' #117 
    
    __Overview__
    
    Initialization
    
    __Technical Comments__
    
    Remove conditional statement which can prevent initialization. 
    
    __Science Comments__
    Minor

--- CMIP6-MOSRS/core/biogeophys/cable_canopy.F90
+++ CMIP6-MOSRS/core/biogeophys/cable_canopy.F90
@@ -631,7 +631,7 @@ CONTAINS
 
 
           ELSEIF( zscl(j) >= rough%zruffs(j) ) THEN
-
+            !Ticket #67 - Modify order of operations to avoid potential error
              r_sc(j) = rough%rt0us(j) + rough%rt1usa(j) + rough%rt1usb(j) +     &
                        ( LOG( ( zscl(j) - rough%disp(j) ) /                     &
                                 MAX( rough%zruffs(j)-rough%disp(j),             &
@@ -2134,15 +2134,9 @@ CONTAINS
                           - (gswminz(i,j)*fwsoilz(i)/C%RGSWC)*cx2z(i,j)*csxz(i,j)
 
 
-                !kdcorbin, 09/10 - new calculations
-                ! no solution, give it a huge number
-                IF( ABS( coef2z(i,j) ) < 1.0e-9 .AND.                           &
-                     ABS( coef1z(i,j) ) < 1.0e-9 ) THEN
-
-                   ciz(i,j) = 99999.0
-                   anrubpz(i,j)  = 99999.0
-
-                ENDIF
+                !Ticket #117 - initialize at all times
+                 ciz(i,j) = 99999.0
+                 anrubpz(i,j)  = 99999.0
 
                 ! solve linearly
                 IF( ABS( coef2z(i,j) ) < 1.e-9 .AND.                            &
