1fbcb4ecfdb0c422134778692c3564f258bfa8c1
Vanessa Haverd <vxh599@nci.org.au>
2017-09-20 04:32:45 +0000

    removed logival attribute of zeroflux in casa_cnpflux subroutine

--- vxh599/trunk_checks_extract_sli_optimise_JVratio/core/biogeochem/casa_inout.F90
+++ vxh599/trunk_checks_extract_sli_optimise_JVratio/core/biogeochem/casa_inout.F90
@@ -1240,11 +1240,11 @@ SUBROUTINE casa_cnpflux(casaflux,casapool,casabal,zeroflux)
   TYPE (casa_flux),    INTENT(INOUT) :: casaflux
   TYPE (casa_pool),    INTENT(INOUT) :: casapool
   TYPE (casa_balance), INTENT(INOUT) :: casabal
-  LOGICAL, OPTIONAL, INTENT(IN) :: zeroflux
+  LOGICAL, INTENT(IN) :: zeroflux
   !  REAL(r_2), INTENT(INOUT) :: clitterinput(mp,3),csoilinput(mp,3)
   INTEGER n
 
-  IF(present(zeroflux) .and. zeroflux) THEN
+  IF( zeroflux) THEN
      casabal%FCgppyear    = 0.0
      casabal%FCrpyear     = 0.0   
      casabal%FCrmleafyear = 0.0
