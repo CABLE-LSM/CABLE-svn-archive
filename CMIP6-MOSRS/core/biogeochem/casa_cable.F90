1e63373ca77edc3add7815b9fecc2b4c9d7834ee
Vanessa Haverd <vxh599@nci.org.au>
2018-02-08 03:34:13 +0000

    addressing tickets 170-171. If statement around reading and writing cable_climate variable from and to dumpfiles.

--- CMIP6-MOSRS/core/biogeochem/casa_cable.F90
+++ CMIP6-MOSRS/core/biogeochem/casa_cable.F90
@@ -378,7 +378,9 @@ SUBROUTINE read_casa_dump(  ncfile, casamet, casaflux,phen, climate, ncall, kend
          phen%doyphase(:,2) = int(phendoyphase2)
          phen%doyphase(:,3) = int(phendoyphase3)
          phen%doyphase(:,4) = int(phendoyphase4)
-         climate%mtemp_max = mtemp
+         IF (cable_user%CALL_climate) THEN 
+            climate%mtemp_max = mtemp
+         ENDIF
          casaflux%Nmindep = Ndep
 
       ENDIF
@@ -513,7 +515,9 @@ SUBROUTINE write_casa_dump( ncfile, casamet, casaflux, phen, climate, n_call, ke
   CALL put_var_ncr2(ncid, var_name(10), real(phen%doyphase(:,2), r_2)    ,n_call )
   CALL put_var_ncr2(ncid, var_name(11), real(phen%doyphase(:,3), r_2)    ,n_call )
   CALL put_var_ncr2(ncid, var_name(12), real(phen%doyphase(:,4), r_2)    ,n_call )
-  CALL put_var_ncr2(ncid, var_name(13), real(climate%mtemp_max,r_2)    ,n_call )
+  IF (cable_user%CALL_climate) THEN 
+     CALL put_var_ncr2(ncid, var_name(13), real(climate%mtemp_max,r_2)    ,n_call )
+  ENDIF
   CALL put_var_ncr2(ncid, var_name(14), real(casaflux%Nmindep,r_2)    ,n_call )
 
 
