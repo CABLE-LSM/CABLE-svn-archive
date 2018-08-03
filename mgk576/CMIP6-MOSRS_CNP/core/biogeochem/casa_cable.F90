0eb3acede90b16c8169d1077d18af9e879998f05
Martin De Kauwe <mgk576@nci.org.au>
2018-08-03 06:50:05 +0000

    added vh vcmax

--- mgk576/CMIP6-MOSRS_CNP/core/biogeochem/casa_cable.F90
+++ mgk576/CMIP6-MOSRS_CNP/core/biogeochem/casa_cable.F90
@@ -604,18 +604,36 @@ END SUBROUTINE write_casa_dump
        !      0.282*log(pleafx(np))*log(nleafx(np))) * 1.0e-6
        nleafx(np) = ncleafx(np)/casabiome%sla(ivt) ! leaf N in g N m-2 leaf
        pleafx(np) = nleafx(np)/npleafx(np) ! leaf P in g P m-2 leaf
-       if (ivt .EQ. 7) then
+       !$       if (ivt .EQ. 7 .OR.ivt .EQ. 9  ) then
+!!$          veg%vcmax(np) = 1.0e-5 ! special for C4 grass: set here to value from  parameter file
+!!$          veg%ejmax(np) = 2.0 * veg%vcmax(np)
+!!$       elseif (ivt.eq.2) then
+!!$          veg%vcmax(np) = vcmax_np(nleafx(np), pleafx(np)) * 1.10
+!!$          veg%ejmax(np) =bjvref * veg%vcmax(np)
+!!$       elseif (ivt.eq.1) then
+!!$          ! account here for spring recovery
+!!$          veg%vcmax(np) = vcmax_np(nleafx(np), pleafx(np))*1.25*climate%frec(np)
+!!$          veg%ejmax(np) =bjvref * veg%vcmax(np)
+!!$       else
+!!$          veg%vcmax(np) = vcmax_np(nleafx(np), pleafx(np))*1.25
+!!$          veg%ejmax(np) =bjvref * veg%vcmax(np)
+!!$       endif
+
+       if (ivt .EQ. 7 .OR.ivt .EQ. 9  ) then
           veg%vcmax(np) = 1.0e-5 ! special for C4 grass: set here to value from  parameter file
+          veg%ejmax(np) = 2.0 * veg%vcmax(np)
+       elseif (ivt.eq.1) then
+           ! account here for spring recovery
+          veg%vcmax(np) = vcmax_np(nleafx(np), pleafx(np))*casabiome%vcmax_scalar(ivt) &
+               *climate%frec(np)
+          veg%ejmax(np) =bjvref * veg%vcmax(np)
        else
-          veg%vcmax(np) = vcmax_np(nleafx(np), pleafx(np))
+          veg%vcmax(np) = vcmax_np(nleafx(np), pleafx(np))*casabiome%vcmax_scalar(ivt)
+          veg%ejmax(np) =bjvref * veg%vcmax(np)
        endif
-    else
-       stop ('invalid vcmax flag')
-    endif
 
-  ENDDO
 
-  veg%ejmax = 2.0 * veg%vcmax
+       !veg%ejmax(np) = 2.0 * veg%vcmax(np)
 !991 format(i6,2x,i4,2x,2(f9.3,2x))
  END SUBROUTINE casa_feedback
 
