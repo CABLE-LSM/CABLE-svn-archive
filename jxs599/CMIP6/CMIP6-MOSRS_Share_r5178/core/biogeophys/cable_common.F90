c8c56a0dbd873ca8fb6b2c2139d6caf02eab75e2
Jhan Srbinovsky <jxs599@nci.org.au>
2018-09-07 03:29:25 +0000

    rollback these changes to start from zero

--- jxs599/CMIP6/CMIP6-MOSRS_Share_r5178/core/biogeophys/cable_common.F90
+++ jxs599/CMIP6/CMIP6-MOSRS_Share_r5178/core/biogeophys/cable_common.F90
@@ -108,10 +108,9 @@ MODULE cable_common_module
           CALL_POP               = .FALSE., & !
           POP_fromZero           = .FALSE., &
           CALL_Climate           = .FALSE., &
-          Climate_fromZero       = .FALSE., &
-          CASA_fromZero          = .FALSE., &
-          POPLUC                 = .FALSE., &
-          limit_labile           = .FALSE.
+          Climate_fromZero       = .TRUE., &
+          CASA_fromZero          = .TRUE., &
+          POPLUC                 = .FALSE.
 
      INTEGER  :: &
           CASA_SPIN_STARTYEAR = 1950, &
