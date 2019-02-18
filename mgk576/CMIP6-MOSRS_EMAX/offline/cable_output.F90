9562b8ddf29df5edb32821be907e36bfe51ceb16
Martin De Kauwe <mgk576@nci.org.au>
2019-02-18 04:01:57 +0000

    Added Annas fixes

--- mgk576/CMIP6-MOSRS_EMAX/offline/cable_output.F90
+++ mgk576/CMIP6-MOSRS_EMAX/offline/cable_output.F90
@@ -1228,18 +1228,18 @@ CONTAINS
                          'froot', '-', 'Fraction of roots in each soil layer', &
                  patchout%froot, soilID, 'soil', xID, yID, zID, landID, patchID)
 
-    IF(output%params .OR. output%slope) CALL define_ovar(ncid_out, opid%slope,   &
-           'slope', '-', 'Mean subgrid topographic slope', &
-                          patchout%slope, 'real', xID, yID, zID, landID, patchID)
-
-    IF(output%params .OR. output%slope_std) CALL define_ovar(ncid_out, opid%slope_std,   &
-           'slope_std', '-', 'Mean subgrid topographic slope_std', &
-                          patchout%slope_std, 'real', xID, yID, zID, landID, patchID)
-
-    IF(output%params .OR. output%GWdz) CALL define_ovar(ncid_out, opid%GWdz,   &
-           'GWdz', '-', 'Mean aquifer layer thickness ', &
-                          patchout%GWdz, 'real', xID, yID, zID, landID, patchID)
-
+!    IF(output%params .OR. output%slope) CALL define_ovar(ncid_out, opid%slope,   &
+!           'slope', '-', 'Mean subgrid topographic slope', &
+!                          patchout%slope, 'real', xID, yID, zID, landID, patchID)
+!
+!    IF(output%params .OR. output%slope_std) CALL define_ovar(ncid_out, opid%slope_std,   &
+!           'slope_std', '-', 'Mean subgrid topographic slope_std', &
+!                          patchout%slope_std, 'real', xID, yID, zID, landID, patchID)
+!
+!    IF(output%params .OR. output%GWdz) CALL define_ovar(ncid_out, opid%GWdz,   &
+!           'GWdz', '-', 'Mean aquifer layer thickness ', &
+!                          patchout%GWdz, 'real', xID, yID, zID, landID, patchID)
+!
     IF(output%params .and. cable_user%gw_model) THEN
            CALL define_ovar(ncid_out, opid%Qhmax,   &
                           'Qhmax', 'mm/s', 'Maximum subsurface drainage ', &
@@ -1467,13 +1467,13 @@ CONTAINS
                            'zse', SPREAD(REAL(soil%zse, 4), 1, mp),ranges%zse, &
                                 patchout%zse, 'soil')! no spatial dim at present
 
-    IF(output%params .OR. output%slope) CALL write_ovar(ncid_out, opid%slope,    &
-                 'slope', REAL(soil%slope, 4), (/0.0,1.0/), patchout%slope, 'real')
-    IF(output%params .OR. output%slope_std) CALL write_ovar(ncid_out, opid%slope_std,    &
-                 'slope_std', REAL(soil%slope_std, 4), (/0.0,1.0/), patchout%slope_std, 'real')
-    IF(output%params .OR. output%GWdz) CALL write_ovar(ncid_out, opid%GWdz,    &
-                 'GWdz', REAL(soil%GWdz, 4), (/0.0,10000.0/), patchout%GWdz, 'real')
-
+!    IF(output%params .OR. output%slope) CALL write_ovar(ncid_out, opid%slope,    &
+!                 'slope', REAL(soil%slope, 4), (/0.0,1.0/), patchout%slope, 'real')
+!    IF(output%params .OR. output%slope_std) CALL write_ovar(ncid_out, opid%slope_std,    &
+!                 'slope_std', REAL(soil%slope_std, 4), (/0.0,1.0/), patchout%slope_std, 'real')
+!    IF(output%params .OR. output%GWdz) CALL write_ovar(ncid_out, opid%GWdz,    &
+!                 'GWdz', REAL(soil%GWdz, 4), (/0.0,10000.0/), patchout%GWdz, 'real')
+!
     IF(output%params .and. cable_user%gw_model) THEN
                   CALL write_ovar(ncid_out, opid%SatFracmax,    &
                        'SatFracmax', spread(REAL(gw_params%MaxSatFraction,4),1,mp), &
@@ -2537,8 +2537,8 @@ CONTAINS
        IF(output%casa) THEN
           out%NPP = out%NPP + REAL(casaflux%cnpp/86400.0 / 1.201E-5, 4)
        ELSE
-          out%NPP = out%NPP + REAL((-1.0 * canopy%fpn - canopy%frp &
-            - casaflux%clabloss/86400.0) / 1.201E-5, 4)
+          out%NPP = out%NPP + REAL((-1.0 * canopy%fpn - canopy%frp))! &
+          !  - casaflux%clabloss/86400.0) / 1.201E-5, 4)
        ENDIF
        IF(writenow) THEN
           ! Divide accumulated variable by number of accumulated time steps:
@@ -2663,7 +2663,7 @@ CONTAINS
    ENDIF
 
     ! NBP and turnover fluxes [umol/m^2/s]
-    IF(output%carbon .OR. output%NBP) THEN
+    IF((output%carbon .OR. output%NBP) .and. output%casa) THEN
        ! Add current timestep's value to total of temporary output variable:
        IF (cable_user%POPLUC) THEN
            out%NBP = out%NBP + -REAL((casaflux%Crsoil-casaflux%cnpp &
