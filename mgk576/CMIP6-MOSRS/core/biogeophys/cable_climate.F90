2fc5d52088f91b443f5063a56e5d2d345407af3d
Martin De Kauwe <mgk576@nci.org.au>
2019-02-21 00:06:05 +0000

    Added missing arg

--- mgk576/CMIP6-MOSRS/core/biogeophys/cable_climate.F90
+++ mgk576/CMIP6-MOSRS/core/biogeophys/cable_climate.F90
@@ -20,7 +20,7 @@
 MODULE cable_climate_mod
 
  Use cable_def_types_mod, ONLY: met_type, climate_type, canopy_type, mp, &
-      r_2, alloc_cbm_var, air_type
+      r_2, alloc_cbm_var, air_type, radiation_type
  USE TypeDef,              ONLY: i4b, dp
 !CABLE_LSM: see CABLE Ticket#149. yet still inclueded file?? legacy-hack??
 # ifndef UM_BUILD
@@ -33,7 +33,7 @@ CONTAINS
 
 
 SUBROUTINE cable_climate(ktau,kstart,kend,ktauday,idoy,LOY,met,climate, canopy, &
-     air, dels, np)
+     air, rad, dels, np)
 
 
   IMPLICIT NONE
@@ -48,6 +48,7 @@ SUBROUTINE cable_climate(ktau,kstart,kend,ktauday,idoy,LOY,met,climate, canopy, 
   TYPE (climate_type), INTENT(INOUT)       :: climate  ! climate variables
   TYPE (canopy_type), INTENT(IN) :: canopy ! vegetation variables
   TYPE (air_type), INTENT(IN)       :: air
+  TYPE (radiation_type), INTENT(IN)  :: rad        ! radiation variables
   REAL, INTENT(IN)               :: dels ! integration time setp (s)
   INTEGER,      INTENT(IN)                  :: np
   INTEGER :: d, y, k
@@ -618,7 +619,7 @@ if (cable_user%climate_fromzero .or. .not.cable_user%call_climate) then
 
 
 else
-   CALL READ_CLIMATE_RESTART_NC (climate)
+   CALL READ_CLIMATE_RESTART_NC (climate, ktauday)
 
 endif
 !else
@@ -871,7 +872,7 @@ STATUS = NF90_PUT_VAR(FILE_ID, VID1(5), climate%qtemp )
 END SUBROUTINE WRITE_CLIMATE_RESTART_NC
 ! ==============================================================================
 
-SUBROUTINE READ_CLIMATE_RESTART_NC ( climate )
+SUBROUTINE READ_CLIMATE_RESTART_NC ( climate, ktauday )
 
   USE netcdf
 
@@ -879,7 +880,7 @@ SUBROUTINE READ_CLIMATE_RESTART_NC ( climate )
   IMPLICIT NONE
 
   TYPE (climate_type), INTENT(INOUT)       :: climate  ! climate variables
-
+  INTEGER, INTENT(IN) :: ktauday
   INTEGER*4 :: mp4
   INTEGER*4, parameter   :: pmp4 =0
   INTEGER, parameter   :: fmp4 = kind(pmp4)
