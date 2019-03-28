99e6cccbf7fc9c93ca32fa8fcb3e81c4662993d4
Martin De Kauwe <mgk576@nci.org.au>
2019-03-28 22:33:10 +0000

    Added ALMA fixes, ticket 212

--- trunk_CNP/offline/cable_input.F90
+++ trunk_CNP/offline/cable_input.F90
@@ -1001,6 +1001,7 @@ SUBROUTINE open_met_file(dels,koffset,kend,spinup, TFRZ)
     ! Look for Rainf (essential):- - - - - - - - - - - - - - - - - -
     IF (ncciy > 0) ncid_met = ncid_rain
     ok = NF90_INQ_VARID(ncid_met,'Rainf',id%Rainf)
+    IF(ok .ne. NF90_NOERR) ok = NF90_INQ_VARID(ncid_met,'Precip',id%Rainf)
     IF(ok /= NF90_NOERR) CALL nc_abort &
          (ok,'Error finding Rainf in met data file ' &
          //TRIM(filename%met)//' (SUBROUTINE open_met_file)')
@@ -1091,6 +1092,7 @@ SUBROUTINE open_met_file(dels,koffset,kend,spinup, TFRZ)
        exists%PSurf = .TRUE. ! PSurf is present in met file
        ! Get PSurf units and check:
        ok = NF90_GET_ATT(ncid_met,id%PSurf,'units',metunits%PSurf)
+       IF(ok .ne. NF90_NOERR) ok = NF90_INQ_VARID(ncid_met, 'Psurf', id%PSurf)
        IF(ok /= NF90_NOERR) CALL nc_abort &
             (ok,'Error finding PSurf units in met data file ' &
             //TRIM(filename%met)//' (SUBROUTINE open_met_file)')
