b33ceee3e78b5ab6b60963239ba3bcdeb73d6120
Martin De Kauwe <mgk576@nci.org.au>
2019-03-30 06:42:05 +0000

    removed bad comma in write statment

--- trunk_CNP/core/biogeochem/casa_cnp.F90
+++ trunk_CNP/core/biogeochem/casa_cnp.F90
@@ -2286,7 +2286,7 @@ SUBROUTINE casa_cnpbal(casapool,casaflux,casabal)
            casaflux%Cnpp(npt)
       write(*,*) 'dcplandt',  casapool%dcplantdt(npt,:), sum(casapool%dcplantdt(npt,:))
       write(*,*) 'rmplant, rgplant',  casaflux%crmplant(npt,:) , casaflux%crgplant(npt)
-      write(*,*), 'dclabile',  casapool%dClabiledt(npt)* deltpool
+      write(*,*) 'dclabile',  casapool%dClabiledt(npt)* deltpool
 
      !  STOP
     ENDIF
