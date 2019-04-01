347c7e36f56519cfd87bda2dfbb9da765e105675
Martin De Kauwe <mgk576@nci.org.au>
2019-04-01 00:07:27 +0000

    Fixed indents

--- mgk576/trunk_fix_indent/core/utils/cable_fprint.F90
+++ mgk576/trunk_fix_indent/core/utils/cable_fprint.F90
@@ -1,117 +1,117 @@
 MODULE cable_fprint_module
 
-   interface cable_fprintf
-      module procedure cable_fprintf0, cable_fprintf1, cable_fprintf2
-      end interface cable_fprintf
+  INTERFACE cable_fprintf
+     MODULE PROCEDURE cable_fprintf0, cable_fprintf1, cable_fprintf2
+  END INTERFACE
 
-   CONTAINS
+CONTAINS
 
-      ! Writes to std output stereamm(=6)
-      !===============================================================================
+  ! Writes to std output stereamm(=6)
+  !===============================================================================
 
-      SUBROUTINE cable_fprintf0( basename, L_fprint )
-         USE cable_common_module, only : knode_gl, ktau_gl
-         implicit none
+  SUBROUTINE cable_fprintf0( basename, L_fprint )
+    USE cable_common_module, ONLY : knode_gl, ktau_gl
+    IMPLICIT NONE
 
-         character(len=*) :: basename     ! subr name
-         logical :: L_fprint
+    CHARACTER(len=*) :: basename     ! subr name
+    LOGICAL :: L_fprint
 
-         if( .NOT. L_fprint ) return
+    IF( .NOT. L_fprint ) RETURN
 
-         call cable_print0( 6, basename, ktau_gl, knode_gl )
+    CALL cable_print0( 6, basename, ktau_gl, knode_gl )
 
-      END SUBROUTINE cable_fprintf0
+  END SUBROUTINE cable_fprintf0
 
-      SUBROUTINE cable_print0(idiag, basename, ktau, mype )
-         implicit none
-         integer :: idiag
-         character(len=*) :: basename     ! subr name
-         integer :: ktau, mype
+  SUBROUTINE cable_print0(idiag, basename, ktau, mype )
+    IMPLICIT NONE
+    INTEGER :: idiag
+    CHARACTER(len=*) :: basename     ! subr name
+    INTEGER :: ktau, mype
 
-         19 format(  "CABLE_LSM: ", A20, " Finished."   )
-         write (idiag, 19)  basename
+19  FORMAT(  "CABLE_LSM: ", A20, " Finished."   )
+    WRITE (idiag, 19)  basename
 
-      END SUBROUTINE cable_print0
+  END SUBROUTINE cable_print0
 
-      !===============================================================================
+  !===============================================================================
 
-      ! Writes to file
-      !===============================================================================
+  ! Writes to file
+  !===============================================================================
 
-      SUBROUTINE cable_fprintf1( iDiag, basename, knode, ktau, L_fprint )
-         USE cable_fFile_module, ONLY : open_file_per_node, fprintf_dir
-         USE cable_common_module, only :
-         implicit none
+  SUBROUTINE cable_fprintf1( iDiag, basename, knode, ktau, L_fprint )
+    USE cable_fFile_module, ONLY : open_file_per_node, fprintf_dir
+    USE cable_common_module, ONLY :
+    IMPLICIT NONE
 
-         integer :: iDiag  ! f^n creates unique unidID to be returned to calling point
-         integer :: ktau, knode
-         ! writes file per processor (basename+node)
-         character(len=*) :: basename     ! subr name
-         logical :: L_fprint
-         ! LOCAL vars
-         integer, SAVE :: pDiag=3713      ! give unique SEED per module procedure
+    INTEGER :: iDiag  ! f^n creates unique unidID to be returned to calling point
+    INTEGER :: ktau, knode
+    ! writes file per processor (basename+node)
+    CHARACTER(len=*) :: basename     ! subr name
+    LOGICAL :: L_fprint
+    ! LOCAL vars
+    INTEGER, SAVE :: pDiag=3713      ! give unique SEED per module procedure
 
-         if( .NOT. L_fprint ) return
+    IF( .NOT. L_fprint ) RETURN
 
-         ! Returns unique unit=iDiag and modified basename
-         call open_file_per_node( iDiag, pDiag, fprintf_dir, basename, knode )
+    ! Returns unique unit=iDiag and modified basename
+    CALL open_file_per_node( iDiag, pDiag, fprintf_dir, basename, knode )
 
-         call cable_print1( idiag, basename, ktau, knode )
+    CALL cable_print1( idiag, basename, ktau, knode )
 
-      END SUBROUTINE cable_fprintf1
+  END SUBROUTINE cable_fprintf1
 
-      SUBROUTINE cable_print1(idiag, basename, ktau, mype )
-         implicit none
-         integer :: idiag
-         character(len=*) :: basename     ! subr name
-         integer :: ktau, mype
+  SUBROUTINE cable_print1(idiag, basename, ktau, mype )
+    IMPLICIT NONE
+    INTEGER :: idiag
+    CHARACTER(len=*) :: basename     ! subr name
+    INTEGER :: ktau, mype
 
-         19 format(  "CABLE_LSM: ", A20, " finished @", I8.1, " on ",I3.1  )
-         write (idiag, 19)  basename, ktau, mype
+19  FORMAT(  "CABLE_LSM: ", A20, " finished @", I8.1, " on ",I3.1  )
+    WRITE (idiag, 19)  basename, ktau, mype
 
-      END SUBROUTINE cable_print1
+  END SUBROUTINE cable_print1
 
-      !===============================================================================
+  !===============================================================================
 
-      ! Writes to file
-      !===============================================================================
-      SUBROUTINE cable_fprintf2( iDiag, basename, msg, L_fprint )
-         USE cable_fFile_module, ONLY : open_file_per_node, fprintf_dir
-         USE cable_common_module, only : knode_gl, ktau_gl
-         implicit none
+  ! Writes to file
+  !===============================================================================
+  SUBROUTINE cable_fprintf2( iDiag, basename, msg, L_fprint )
+    USE cable_fFile_module, ONLY : open_file_per_node, fprintf_dir
+    USE cable_common_module, ONLY : knode_gl, ktau_gl
+    IMPLICIT NONE
 
-         integer :: iDiag  ! f^n creates unique unidID to be returned to calling point
-         ! writes file per processor (basename+node)
-         character(len=*) :: basename     ! subr name
-         character(len=*) :: msg ! message
-         logical :: L_fprint
-         ! LOCAL vars
-         integer, SAVE :: pDiag=5713      ! give unique SEED per module procedure
+    INTEGER :: iDiag  ! f^n creates unique unidID to be returned to calling point
+    ! writes file per processor (basename+node)
+    CHARACTER(len=*) :: basename     ! subr name
+    CHARACTER(len=*) :: msg ! message
+    LOGICAL :: L_fprint
+    ! LOCAL vars
+    INTEGER, SAVE :: pDiag=5713      ! give unique SEED per module procedure
 
-         if( .NOT. L_fprint ) return
+    IF( .NOT. L_fprint ) RETURN
 
-         ! Returns unique unit=iDiag and modified basename
-         call open_file_per_node( iDiag, pDiag, fprintf_dir, basename, knode_gl )
+    ! Returns unique unit=iDiag and modified basename
+    CALL open_file_per_node( iDiag, pDiag, fprintf_dir, basename, knode_gl )
 
-         call cable_print2( idiag, basename, msg, ktau_gl, knode_gl )
+    CALL cable_print2( idiag, basename, msg, ktau_gl, knode_gl )
 
-      END SUBROUTINE cable_fprintf2
+  END SUBROUTINE cable_fprintf2
 
-      SUBROUTINE cable_print2(idiag, basename, msg, ktau, mype )
-         implicit none
-         integer :: idiag
-         character(len=*) :: basename     ! subr name
-         character(len=*) :: msg ! message
-         integer :: ktau, mype
+  SUBROUTINE cable_print2(idiag, basename, msg, ktau, mype )
+    IMPLICIT NONE
+    INTEGER :: idiag
+    CHARACTER(len=*) :: basename     ! subr name
+    CHARACTER(len=*) :: msg ! message
+    INTEGER :: ktau, mype
 
-         write (idiag, *) ""
-         write (idiag, *) msg
-         write (idiag, *) &
+    WRITE (idiag, *) ""
+    WRITE (idiag, *) msg
+    WRITE (idiag, *) &
          "lsm_id set CABLE. call succeeded however return and call JULES"
-         write (idiag, *) ""
+    WRITE (idiag, *) ""
 
-      END SUBROUTINE cable_print2
+  END SUBROUTINE cable_print2
 
 
 
-   END MODULE cable_fprint_module
+END MODULE cable_fprint_module
