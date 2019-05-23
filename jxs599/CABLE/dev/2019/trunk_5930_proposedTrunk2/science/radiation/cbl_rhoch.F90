a2bef8ff73ffd8e5e871aef5c29a4358bdc870f2
Jhan Srbinovsky <jxs599@nci.org.au>
2019-05-23 01:29:27 +0000

    HaC changes from trunk - remove as needed

---
+++ jxs599/CABLE/dev/2019/trunk_5930_proposedTrunk2/science/radiation/cbl_rhoch.F90
@@ -0,0 +1,32 @@
+MODULE cbl_rhoch_module
+
+   IMPLICIT NONE
+
+   PUBLIC calc_rhoch
+   PRIVATE
+
+CONTAINS
+
+! this subroutine called from _init_radiation on cable_albedo.F90 pathway, explicit and implict
+SUBROUTINE calc_rhoch( c1,rhoch, mp, nrb, taul, refl )
+
+   USE cable_def_types_mod, ONLY : veg_parameter_type
+
+integer :: mp
+integer :: nrb
+   REAL :: c1(mp,nrb)
+   REAL :: rhoch(mp,nrb)
+   REAL :: taul(mp,nrb)
+   REAL :: refl(mp,nrb)
+
+   c1(:,1) = SQRT(1. - taul(:,1) - refl(:,1))
+   c1(:,2) = SQRT(1. - taul(:,2) - refl(:,2))
+   c1(:,3) = 1.
+
+   ! Canopy C%REFLection black horiz leaves
+   ! (eq. 6.19 in Goudriaan and van Laar, 1994):
+   rhoch = (1.0 - c1) / (1.0 + c1)
+
+END SUBROUTINE calc_rhoch
+
+END MODULE cbl_rhoch_module
