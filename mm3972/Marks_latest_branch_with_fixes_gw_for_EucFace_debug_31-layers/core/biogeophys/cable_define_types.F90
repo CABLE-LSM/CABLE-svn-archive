9ef794941cb5e2a86f34d1fdc80e02118ee8cb58
Mengyuan Mu <mm3972@nci.org.au>
2019-08-18 07:06:24 +0000

    use 31 soil layers and init GWwb = 0.9*GWssat

--- mm3972/Marks_latest_branch_with_fixes_gw_for_EucFace_debug_31-layers/core/biogeophys/cable_define_types.F90
+++ mm3972/Marks_latest_branch_with_fixes_gw_for_EucFace_debug_31-layers/core/biogeophys/cable_define_types.F90
@@ -55,7 +55,8 @@ MODULE cable_def_types_mod
       niter = 4,     & ! number of iterations for za/L
  !      ms = 12          ! # soil layers
  !      ms = 6         ! # soil layers - standard ! MMY
-       ms = 13          ! for Loetschental experiment ! MMY
+ !      ms = 13          ! for Loetschental experiment ! MMY
+      ms = 31          ! for EucFACE experiment ! MMY
 
 !   PRIVATE :: r_2, ms, msn, mf, nrb, ncp, ncs
 
