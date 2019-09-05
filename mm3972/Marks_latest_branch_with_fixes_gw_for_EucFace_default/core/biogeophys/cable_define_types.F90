46bed7e1eb0bdb112104151bc45618b760368a57
Mengyuan Mu <mm3972@nci.org.au>
2019-09-05 10:44:42 +0000

    ms = 13

--- mm3972/Marks_latest_branch_with_fixes_gw_for_EucFace_default/core/biogeophys/cable_define_types.F90
+++ mm3972/Marks_latest_branch_with_fixes_gw_for_EucFace_default/core/biogeophys/cable_define_types.F90
@@ -54,9 +54,9 @@ MODULE cable_def_types_mod
       swb = 2,       & ! # shortwave bands
       niter = 4,     & ! number of iterations for za/L
  !      ms = 12          ! # soil layers
-       ms = 6         ! # soil layers - standard
-!       ms = 13          ! for Loetschental experiment
-
+!       ms = 6         ! # soil layers - standard
+       ms = 13          ! for Loetschental experiment
+!       ms = 31 
 !   PRIVATE :: r_2, ms, msn, mf, nrb, ncp, ncs
 
 ! .............................................................................
