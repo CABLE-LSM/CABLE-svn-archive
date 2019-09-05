7f1183983151995ae482357b39fa794260009278
Mengyuan Mu <mm3972@nci.org.au>
2019-09-05 05:07:19 +0000

    put back soil%hyds_vec(:,klev) = soil%hyds_vec(:,klev)*exp(-soil%hkrz(:)*(soil_depth(:,klev)-soil%zdepth(:)))

--- mm3972/Marks_latest_branch_with_fixes_gw_for_EucFace_debug_31-layers/offline/cable_parameters.F90
+++ mm3972/Marks_latest_branch_with_fixes_gw_for_EucFace_debug_31-layers/offline/cable_parameters.F90
@@ -1761,11 +1761,11 @@ CONTAINS
 
        ELSE
           ! _______________________ MMY ____________________________
-          !DO klev=1,ms
-          !    soil%hyds_vec(:,klev) = soil%hyds_vec(:,klev)*exp(-soil%hkrz(:)*(soil_depth(:,klev)-soil%zdepth(:)))
-          !END DO
+          DO klev=1,ms
+              soil%hyds_vec(:,klev) = soil%hyds_vec(:,klev)*exp(-soil%hkrz(:)*(soil_depth(:,klev)-soil%zdepth(:)))
+          END DO
           ! ________________________________________________________
-          PRINT *, "comment out soil%hyds_vec(:,klev) = soil%hyds_vec(:,klev)*exp(-soil%hkrz(:)*(soil_depth(:,klev)-soil%zdepth(:)))" ! MMY
+          ! PRINT *, "comment out soil%hyds_vec(:,klev) = soil%hyds_vec(:,klev)*exp(-soil%hkrz(:)*(soil_depth(:,klev)-soil%zdepth(:)))" ! MMY
 
        END IF  !use either uni or multi cosby transfer func
 
