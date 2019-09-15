fafa8c731319c80e39eeea3226072c56287093fe
Martin De Kauwe <mgk576@nci.org.au>
2019-09-15 06:54:32 +0000

    put back duplicate

--- mgk576/trunk/offline/cable_mpiworker.F90
+++ mgk576/trunk/offline/cable_mpiworker.F90
@@ -2422,9 +2422,9 @@ CONTAINS
     CALL MPI_Get_address (soil%slope_std, displs(bidx), ierr)
     blen(bidx) = r2len
 
-    !bidx = bidx + 1
-    !CALL MPI_Get_address (soil%GWdz, displs(bidx), ierr)
-    !blen(bidx) = r2len
+    bidx = bidx + 1
+    CALL MPI_Get_address (soil%GWdz, displs(bidx), ierr)
+    blen(bidx) = r2len
 
     bidx = bidx + 1
     CALL MPI_Get_address (ssnow%GWwb, displs(bidx), ierr)
