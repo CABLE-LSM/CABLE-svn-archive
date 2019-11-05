bebc2503e3d17c8dcd4b26e0748bb9efaa239a7e
Jhan Srbinovsky <jxs599@nci.org.au>
2019-11-05 04:43:05 +0000

    update/include extra params used in JAC,HAC

--- jxs599/CABLE/dev/2019/Merge226intoQuasitrunkFor_226/params/cbl_phys_constants_mod.F90
+++ jxs599/CABLE/dev/2019/Merge226intoQuasitrunkFor_226/params/cbl_phys_constants_mod.F90
@@ -12,7 +12,10 @@ PUBLIC
 ! This file belongs in 
 !-----------------------------------------------------------------------------
 
-  REAL,    PARAMETER :: tfrz   = 273.16   ! Temp (K) corresp. to 0 C
-
+  REAL, PARAMETER :: tfrz   = 273.16     ! Temp (K) corresp. to 0 C
+  REAL, PARAMETER :: sboltz = 5.67e-8    ! Stefan-Boltz. constant (W/m2/K4)
+  REAL, PARAMETER :: emsoil = 1.0        ! soil emissivity
+  REAL, PARAMETER :: emleaf = 1.0        ! leaf emissivity
+  REAL, PARAMETER :: capp   = 1004.64    ! air spec. heat (J/kg/K)
 
 END MODULE cable_phys_constants_mod
