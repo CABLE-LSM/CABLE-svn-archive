dc4b51a3ee960ed124f9be8ff2c5f363ff59dc18
Mengyuan Mu <mm3972@nci.org.au>
2020-01-06 23:43:43 +0000

    set init GWwb = wb(ms)

--- mm3972/Marks_latest_branch_with_fixes_gw_for_EucFace_fix_met_multi-layer/offline/cable_input.F90
+++ mm3972/Marks_latest_branch_with_fixes_gw_for_EucFace_fix_met_multi-layer/offline/cable_input.F90
@@ -2858,7 +2858,8 @@ SUBROUTINE get_parameters_met(soil,ssnow,veg,bgc,rough,completeSet) ! MMY add ss
    !!!!!ssnow%GWwb(:)      = ssnow%wb(:,ms)
    soil%GWhyds_vec(:) = soil%hyds_vec(:,ms)
    soil%GWssat_vec(:) = soil%ssat_vec(:,ms)
-   ssnow%GWwb(:)      = soil%GWssat_vec(:) * 0.9
+   !ssnow%GWwb(:)      = soil%GWssat_vec(:) * 0.9
+   ssnow%GWwb(:)      = soil%wb(:,ms)
    ! Set init GWwb as 0.9 ssat to avoid the aquifer saturates too quick
    soil%GWsucs_vec(:) = soil%sucs_vec(:,ms)
    soil%GWbch_vec(:)  = soil%bch_vec(:,ms)
