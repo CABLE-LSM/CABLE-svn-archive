7d725dff7911806254ce3e7c5c9fbe0973377805
Martin De Kauwe <mgk576@nci.org.au>
2020-02-14 04:06:17 +0000

    Fixed bug

--- mgk576/trunk_analytical/core/biogeochem/spincasacnp.F90
+++ mgk576/trunk_analytical/core/biogeochem/spincasacnp.F90
@@ -214,9 +214,9 @@ SUBROUTINE spincasacnp( dels,kstart,kend,mloop,veg,soil,casabiome,casapool, &
         nptx=8173
 
         ! Calculate average allocation fractions  for the plant pools
-        avg_af = avg_ar + casaflux%fracCalloc(:,leaf)
-        avg_aw = avg_ar + casaflux%fracCalloc(:,wood)
-        avg_ar = avg_aw + casaflux%fracCalloc(:,froot)
+        avg_af = avg_af + casaflux%fracCalloc(:,leaf)
+        avg_aw = avg_aw + casaflux%fracCalloc(:,wood)
+        avg_ar = avg_ar + casaflux%fracCalloc(:,froot)
 
         ! Calculate average turnover fractions for the plant pools
         avg_lf = avg_lf + (1.0 / casaflux%kplant(:,leaf))
