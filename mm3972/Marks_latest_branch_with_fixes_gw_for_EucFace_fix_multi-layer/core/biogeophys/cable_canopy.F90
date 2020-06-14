f68c68ad7ca0e68a558aedfc6915c2e5ffe4d185
Mengyuan Mu <mm3972@nci.org.au>
2020-06-14 02:28:19 +0000

    test sensitivity to alpha and convex

--- mm3972/Marks_latest_branch_with_fixes_gw_for_EucFace_fix_multi-layer/core/biogeophys/cable_canopy.F90
+++ mm3972/Marks_latest_branch_with_fixes_gw_for_EucFace_fix_multi-layer/core/biogeophys/cable_canopy.F90
@@ -1940,8 +1940,8 @@ CONTAINS
              ! PRINT *, "====================================="
 
              ! prescribe plant photosynthesis parameters
-             veg%alpha = 0.3
-             veg%convex = 0.48
+             ! veg%alpha = 0.3
+             ! veg%convex = 0.48
              veg%g1 = 5.
              veg%vcmax = 91e-6
              veg%ejmax = 159e-6
