711ac9e3a316ce1855fd4faa1285c2480761da87
Martin De Kauwe <mgk576@nci.org.au>
2020-09-14 02:42:45 +0000

    added b,c to met file

--- mgk576/profit_max/core/biogeophys/cable_canopy.F90
+++ mgk576/profit_max/core/biogeophys/cable_canopy.F90
@@ -2132,8 +2132,7 @@ CONTAINS
                    anx(i,1) = 0.0 - rdx(i,1)
                    anx(i,2) = 0.0 - rdx(i,2)
                 ELSE
-                   print*, veg%b_plant(i), veg%c_plant(i), veg%vcmax(i)*1e6
-                   stop
+
                    CALL optimisation(canopy, rad%qcan, vpd, press, tlfx(i), &
                                      csx, rad%fvlai, &
                                      ssnow%weighted_psi_soil(i), &
