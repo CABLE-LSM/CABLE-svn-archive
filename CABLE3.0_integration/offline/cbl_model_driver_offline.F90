b599f106e061c8e823c4c653a4793c034739444d
Jhan Srbinovsky <jxs599@nci.org.au>
2020-11-19 12:29:07 +0000

    so this has been reviewed to include chanes in offline that we think will be necessary to maintain equiv with JAC-Loobos

--- CABLE3.0_integration/offline/cbl_model_driver_offline.F90
+++ CABLE3.0_integration/offline/cbl_model_driver_offline.F90
@@ -7,8 +7,7 @@ PUBLIC cbm
 
 CONTAINS
 
-SUBROUTINE cbm( ktau,dels, air, &
-                    bgc, canopy, met,                                &
+SUBROUTINE cbm( ktau,dels, air, bgc, canopy, met,                                &
                     bal, rad, rough, soil,                                      &
                     ssnow, sum_flux, veg, climate )
 
@@ -42,13 +41,6 @@ USE cable_math_constants_mod, ONLY : CPI180 => pi180
 use cbl_masks_mod, ONLY :  fveg_mask,  fsunlit_mask,  fsunlit_veg_mask
 use cbl_masks_mod, ONLY :  veg_mask,  sunlit_mask,  sunlit_veg_mask
 
-  !diag 
-  USE cable_fprint_module, ONLY : cable_fprintf
-  USE cable_Pyfprint_module, ONLY : cable_Pyfprintf
-  USE cable_fFile_module, ONLY : fprintf_dir_root, fprintf_dir, L_cable_fprint,&
-                                 L_cable_Pyfprint, unique_subdir, qprint
-  !H!USE cable_def_types_mod
-  !H!USE cable_fprint_type_mod, ONLY : fprintx 
     ! CABLE model variables
     TYPE (air_type),       INTENT(INOUT) :: air
     TYPE (bgc_pool_type),  INTENT(INOUT) :: bgc
@@ -67,7 +59,6 @@ use cbl_masks_mod, ONLY :  veg_mask,  sunlit_mask,  sunlit_veg_mask
     REAL, INTENT(IN)               :: dels ! time setp size (s)
     INTEGER, INTENT(IN) :: ktau
     INTEGER :: k,kk,j
-    LOGICAL, SAVE :: first_call = .TRUE.
 character(len=*), parameter :: subr_name = "cbm"
 LOGICAL :: cbl_standalone= .true.
 LOGICAL :: jls_standalone= .false.
@@ -86,20 +77,6 @@ REAL :: c1(mp,nrb)
 REAL :: rhoch(mp,nrb)
 REAL :: xk(mp,nrb)
 
-# include "cable_fprint.txt"
-cntile =1
-IF ( first_call ) THEN 
-  ALLOCATE( fprintx% fprint1(mp) )
-  ALLOCATE( fprintx% fprint2(mp) )
-  ALLOCATE( fprintx% fprint3(mp) )
-  ALLOCATE( fprintx% fprint4(mp) )
-  ALLOCATE( fprintx% fprint5(mp) )
-  ALLOCATE( fprintx% fprint6(mp) )
-  ALLOCATE( fprintx% fprint7(mp) )
-  ALLOCATE( fprintx% fprint8(mp) )
-  ALLOCATE( fprintx% fprint9(mp) )
-EndIf
- 
 !iFor testing
 ICYCLE = 0
 cable_user%soil_struc="default"
@@ -159,12 +136,10 @@ CALL init_radiation( rad%extkb, rad%extkd,                                     &
                rad%reffdf, rad%reffbm                                        &
              ) !EffSurfRefl_dif, EffSurfRefl_beam 
 
-!CABLE_LSM:check
-!IF ( explicit_path ) THEN
+!Otherwise this is only initialized for CABLE-offline IO
 IF ( ktau_gl == 1) THEN
   ssnow%tss=(1 - ssnow%isflag) * ssnow%tgg(:,1) + ssnow%isflag * ssnow%tggsn(:,1)
        ssnow%otss = ssnow%tss
-       first_call = .FALSE.
 ENDIF
 ssnow%otss_0 = ssnow%otss  ! vh should be before call to canopy?
 ssnow%otss = ssnow%tss
@@ -190,140 +165,18 @@ canopy%rnet = canopy%fns + canopy%fnv
        rad%trad = ( ( 1.-rad%transd ) * canopy%tv**4 +                             &
             rad%transd * ssnow%tss**4 )**0.25
 
-    !H!   !calculate canopy%frp
-    !H!   CALL plantcarb(veg,bgc,met,canopy)
+    !calculate canopy%frp
+    CALL plantcarb(veg,bgc,met,canopy)
 
-    !H!   !calculate canopy%frs
-    !H!   CALL soilcarb(soil, ssnow, veg, bgc, met, canopy)
+    !calculate canopy%frs
+    CALL soilcarb(soil, ssnow, veg, bgc, met, canopy)
 
-    !H!   CALL carbon_pl(dels, soil, ssnow, veg, canopy, bgc)
+    CALL carbon_pl(dels, soil, ssnow, veg, canopy, bgc)
 
-    !H!   canopy%fnpp = -1.0* canopy%fpn - canopy%frp
-    !H!   canopy%fnee = canopy%fpn + canopy%frs + canopy%frp
-
-    !H!ENDIF
-
-fprintf_dir="/home/599/jxs599/TestHAC5.7_offl/"
-!H!#forcing
-!H!vname='met%tk'; dimx=1 
-!H!diagar(1) = met%tk(cntile)
-!H!call cable_Pyfprintf( cDiag16, vname, diagar, dimx, .true.)
-!H!
-!H!vname='met%fsd1'; dimx=1 
-!H!diagar(1) = met%fsd(cntile,1)
-!H!call cable_Pyfprintf( cDiag17, vname, diagar, dimx, .true.)
-!H!
-!H!vname='met%fsd2'; dimx=1 
-!H!diagar(1) = met%fsd(cntile,2)
-!H!call cable_Pyfprintf( cDiag18, vname, diagar, dimx, .true.)
-!H!
-!H!vname='met%fld'; dimx=1 
-!H!diagar(1) = met%fld(cntile)
-!H!call cable_Pyfprintf( cDiag19, vname, diagar, dimx, .true.)
-!H!
-!H!vname='met%precip'; dimx=1 
-!H!diagar(1) = met%precip(cntile)
-!H!call cable_Pyfprintf( cDiag20, vname, diagar, dimx, .true.)
-!H!
-!H!vname='met%precip_sn'; dimx=1 
-!H!diagar(1) = met%precip_sn(cntile)
-!H!call cable_Pyfprintf( cDiag21, vname, diagar, dimx, .true.)
-!H!
-!H!vname='met%ua'; dimx=1 
-!H!diagar(1) = met%ua(cntile)
-!H!call cable_Pyfprintf( cDiag22, vname, diagar, dimx, .true.)
-!H!
-!H!vname='met%pmb'; dimx=1 
-!H!diagar(1) = met%pmb(cntile)
-!H!call cable_Pyfprintf( cDiag23, vname, diagar, dimx, .true.)
-!H!
-!H!vname='met%qv'; dimx=1 
-!H!diagar(1) = met%qv(cntile)
-!H!call cable_Pyfprintf( cDiag24, vname, diagar, dimx, .true.)
-
-vname='canopy%fhv'; dimx=1 
-diagar(1) = canopy%fhv(cntile)
-call cable_Pyfprintf( cDiag1, vname, diagar, dimx, .true.)
-
-vname='canopy%fhs'; dimx=1 
-diagar(1) = canopy%fhs(cntile)
-call cable_Pyfprintf( cDiag2, vname, diagar, dimx, .true.)
-
-vname='canopy%fes'; dimx=1 
-diagar(1) = canopy%fes(cntile)
-call cable_Pyfprintf( cDiag3, vname, diagar, dimx, .true.)
-
-vname='canopy%fev'; dimx=1 
-diagar(1) = canopy%fev(cntile)
-call cable_Pyfprintf( cDiag4, vname, diagar, dimx, .true.)
-
-vname='canopy%fess'; dimx=1 
-diagar(1) = canopy%fess(cntile)
-call cable_Pyfprintf( cDiag5, vname, diagar, dimx, .true.)
-
-vname='canopy%fesp'; dimx=1 
-diagar(1) = canopy%fesp(cntile)
-call cable_Pyfprintf( cDiag6, vname, diagar, dimx, .true.)
-
-vname='ssnow%wetfac'; dimx=1 
-diagar(1) = ssnow%wetfac(cntile)
-call cable_Pyfprintf( cDiag6, vname, diagar, dimx, .true.)
-
-vname='ssnow%potev'; dimx=1 
-diagar(1) = ssnow%potev(cntile)
-call cable_Pyfprintf( cDiag8, vname, diagar, dimx, .true.)
-
-vname='ssnow%wb'; dimx=1 
-diagar(1) = (0.355 * ssnow%wb(1,1) ) + ( 0.355 * ssnow%wb(2,1) ) + ( 0.208 * ssnow%wb(3,1) ) + ( 0.082 * ssnow%wb(4,1) )
-call cable_Pyfprintf( cDiag9, vname, diagar, dimx, .true.)
-
-vname='Albedo'; dimx=1 
-diagar(1) = rad%reffdf(cntile,1) + rad%reffbm(cntile,1)
-                                        &
-call cable_Pyfprintf( cDiag10, vname, diagar, dimx, .true.)
-
-!for soilsnow_main?
-!
-!vname='mwbice'; dimx=1 
-!diagar(1) = fprintx%fprint3(cntile)
-!call cable_Pyfprintf( cDiag29, vname, diagar, dimx, .true.)
-!
-!vname='mwblf'; dimx=1 
-!diagar(1) = fprintx%fprint4(cntile)
-!call cable_Pyfprintf( cDiag31, vname, diagar, dimx, .true.)
-!
-!vname='mwbfice'; dimx=1 
-!diagar(1) = fprintx%fprint5(cntile)
-!call cable_Pyfprintf( cDiag32, vname, diagar, dimx, .true.)
-!
-!vname='mwb'; dimx=1 
-!diagar(1) = fprintx%fprint6(cntile)
-!call cable_Pyfprintf( cDiag33, vname, diagar, dimx, .true.)
-!??
-!vname='mgammzz'; dimx=1 
-!diagar(1) = fprintx%fprint2(cntile)
-!call cable_Pyfprintf( cDiag23, vname, diagar, dimx, .true.)
+    canopy%fnpp = -1.0* canopy%fpn - canopy%frp
+    canopy%fnee = canopy%fpn + canopy%frs + canopy%frp
 
 
-
-!!IF ( cable_runtime%um_explicit ) then 
-!!fprintf_dir="/home/599/jxs599/TestHAC5.7_HAC_expl/"
-!!vname='canopy%fhv'; dimx=1 
-!!diagar(1) = canopy%fhv(cntile)
-!!call cable_Pyfprintf( cDiag1, vname, diagar, dimx, .true.)
-!!vname='canopy%fhs'; dimx=1 
-!!diagar(1) = canopy%fhs(cntile)
-!!call cable_Pyfprintf( cDiag2, vname, diagar, dimx, .true.)
-!!vname='canopy%fes'; dimx=1 
-!!diagar(1) = canopy%fes(cntile)
-!!call cable_Pyfprintf( cDiag3, vname, diagar, dimx, .true.)
-!!vname='canopy%fev'; dimx=1 
-!!diagar(1) = canopy%fev(cntile)
-!!call cable_Pyfprintf( cDiag4, vname, diagar, dimx, .true.)
-!!vname='met%tk'; dimx=1 
-!!diagar(1) = met%tk(cntile)
-!!call cable_Pyfprintf( cDiag5, vname, diagar, dimx, .true.)
-!!endif
 
 
 
