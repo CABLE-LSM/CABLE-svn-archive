bd35f8e61500146d76b4af7710b56b77e489884b
Jhan Srbinovsky <jxs599@nci.org.au>
2021-01-04 11:45:36 +0000

    works - no changes - matches ReBASE@7846. Improoved calc of toplevel masks

--- jxs599/CABLE/dev/2020/CABLE-3.0forMPI_Xmas/util/cbl_masks.F90
+++ jxs599/CABLE/dev/2020/CABLE-3.0forMPI_Xmas/util/cbl_masks.F90
@@ -23,10 +23,15 @@ LOGICAL, allocatable :: veg_mask(:)
 integer ::  mp
 real :: lai_thresh
 real :: reducedLAIdue2snow(mp)
+integer :: i
  
 IF ( .NOT. ALLOCATED(veg_mask)) ALLOCATE( veg_mask(mp) )
 
-  veg_mask = reducedLAIdue2snow > lai_thresh 
+veg_mask = .FALSE.
+! Define vegetation mask:
+do i=1, mp  
+  if( reducedLAIdue2snow(i) > lai_thresh ) veg_mask(i) = .true.
+end do
 
 End subroutine fveg_mask
 
@@ -39,12 +44,16 @@ LOGICAL, allocatable :: sunlit_mask(:)
 integer ::  mp
 real :: coszen_tols
 real :: coszen(mp)   
+integer :: i
 
-  IF ( .NOT. ALLOCATED(sunlit_mask)) ALLOCATE( sunlit_mask(mp) )
+IF ( .NOT. ALLOCATED(sunlit_mask)) ALLOCATE( sunlit_mask(mp) )
 
-  ! Define sunlit mask:
-  sunlit_mask =  coszen(:) > coszen_tols
-   
+sunlit_mask = .FALSE.
+! Define sunlit mask:
+do i=1, mp  
+  if( coszen(i) > coszen_tols ) sunlit_mask(i) = .true.  
+end do
+
 End subroutine fsunlit_mask
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -54,11 +63,15 @@ subroutine fsunlit_veg_mask( sunlit_veg_mask, mp )
 implicit none
 LOGICAL, allocatable :: sunlit_veg_mask(:)
 integer ::  mp
+integer :: i
 
-  IF ( .NOT. ALLOCATED(sunlit_veg_mask)) ALLOCATE( sunlit_veg_mask(mp) )
+IF ( .NOT. ALLOCATED(sunlit_veg_mask) ) ALLOCATE( sunlit_veg_mask(mp) )
 
-  ! Define sunlit AND vegetation mask:
-  sunlit_veg_mask = veg_mask .AND.  sunlit_mask
+sunlit_veg_mask = .FALSE.
+! Define sunlit AND vegetation mask:
+do i=1, mp  
+  if( veg_mask(i) .AND.  sunlit_mask(i) ) sunlit_veg_mask(i) = .true.
+end do
 
 End subroutine fsunlit_veg_mask
 
