cc810439709626b0b18ce4c8a0d82e0436c357f1
Yingping Wang <yxw599@nci.org.au>
2021-09-17 00:52:25 +0000

    ! include "cable_abort.o" when compiling landuse_inout.F90

--- yxw599/cable-3.0_295/offline/landuse_inout.F90
+++ yxw599/cable-3.0_295/offline/landuse_inout.F90
@@ -1,5 +1,6 @@
   subroutine landuse_data(mlon,mlat,landmask,arealand,luc_atransit,luc_fharvw,luc_xluh2cable)
   use netcdf
+  use cable_abort_module,   ONLY: nc_abort
   use cable_common_module,  ONLY: filename
   USE cable_def_types_mod,  ONLY: mland,r_2
   use landuse_constant,     ONLY: mstate,mvmax,mharvw
@@ -70,6 +71,7 @@
 SUBROUTINE landuse_getxluh2(mlat,mlon,landmask,fxluh2cable,luc_xluh2cable)
 ! get data: luc%fprimary; luc%fsecondary
   USE netcdf
+  use cable_abort_module,   ONLY: nc_abort
   USE cable_def_types_mod,  ONLY: mland,r_2
   use landuse_constant,     ONLY: mstate,mvmax,mharvw
   IMPLICIT NONE
@@ -113,6 +115,7 @@ SUBROUTINE landuse_getxluh2(mlat,mlon,landmask,fxluh2cable,luc_xluh2cable)
 SUBROUTINE landuse_getdata(mlat,mlon,landmask,fxpft,luc_atransit,luc_fharvw)
 ! get LUC data
   USE netcdf
+  use cable_abort_module,   ONLY: nc_abort
   USE cable_def_types_mod,  ONLY: mland,r_2
   use landuse_constant,     ONLY: mstate,mvmax,mharvw
   IMPLICIT NONE
