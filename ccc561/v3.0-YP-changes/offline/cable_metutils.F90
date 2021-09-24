17d8e4f9ee99ae90fd322680e4baa7002ac0df06
Claire Carouge <ccc561@nci.org.au>
2021-09-24 07:58:53 +0000

    Updates from Metvar-lookup1 for benchmarking

--- ccc561/v3.0-YP-changes/offline/cable_metutils.F90
+++ ccc561/v3.0-YP-changes/offline/cable_metutils.F90
@@ -100,8 +100,8 @@ module CABLE_METUTILS_MODULE
                         /)
     
   ! Surface pressure variable
-  CHARACTER(LEN=5),DIMENSION(4) :: MetPSurfNames=(/                           &
-                        'pres ', 'ps   ', 'PBOT ', 'PSurf'                    &
+  CHARACTER(LEN=5),DIMENSION(5) :: MetPSurfNames=(/                           &
+                        'pres ', 'ps   ', 'PBOT ', 'PSurf','Psurf'            &
                         /)
     
   ! Elevation variable
