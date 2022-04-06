b33d3d437c47e9d5d9b8ffa2b4063a93c19af4cf
Ian Harman <inh599@nci.org.au>
2022-04-06 04:55:52 +0000

    moves snow_age evaluation from albedo to soilsnow: 1st attempt

---
+++ inh599/move_snowage_Ticket331/science/soilsnow/cbl_snow_aging.F90
@@ -0,0 +1,90 @@
+MODULE snow_aging_mod
+
+  !This routine evaluates the effecive age of the snow pack
+  !It is called from soilsnow_main and output SnowAge is used in cbl_snow_albedo
+
+PUBLIC snow_aging
+
+CONTAINS
+
+  SUBROUTINE snow_aging(SnowAge,mp,dels,SnowDepth,SnowODepth,SnowTemp, &
+       SoilTemp,SnowFlag_3L, surface_type,soil_type)
+
+    !need to eliminate this USE data statements  
+    USE cable_phys_constants_mod, ONLY : CTFRZ => TFRZ
+
+    IMPLICIT NONE
+
+    REAL, INTENT(OUT)   :: SnowAge(mp)
+    INTEGER, INTENT(IN) :: mp
+    REAL, INTENT(IN)    :: dels
+    REAL, INTENT(IN)    :: SnowDepth(mp)
+    REAL, INTENT(IN)    :: SnowODepth(mp)
+    REAL, INTENT(IN)    :: SnowTemp(mp)
+    REAL, INTENT(IN)    :: SoilTemp(mp)
+    INTEGER, INTENT(IN) :: SnowFlag_3L(mp)
+    INTEGER, INTENT(IN) :: surface_type(mp) 
+    INTEGER, INTENT(IN) :: soil_type(mp) 
+
+    !hard wired index to be eliminated
+    INTEGER, PARAMETER :: perm_ice = 9
+    !model parameter shared across subroutines -> cable_phys_constants
+    REAL, PARAMETER :: snow_depth_thresh = 1.0
+
+    !working variables
+    REAL, DIMENSION(mp) ::                             &
+         ar1,     &  ! factor for crystal growth  (-ve)
+         ar2,     &  ! factor for freezing of melt water
+         ar3,     &  ! factor for accumulation of dirt
+         dnsnow,  &  ! depth of new snow albedo
+         dtau        !change in effective snow age
+
+    INTEGER :: i        !looping variable
+    REAL    :: tmp      !local effective temperauture
+
+    !default to values if no snow
+    ar1(:)=0.0
+    ar2(:)=0.0
+    ar3(:)=0.0
+    DO i=1,mp
+       IF (SnowDepth(i)>snow_depth_thresh) THEN
+
+          !depth of new snow (in cm H20)
+          dnsnow(i) = MIN (1.0, .1 * MAX( 0., SnowDepth(i) - SnowODepth(i) ) )
+
+          ! Snow age depends on snow crystal growth, freezing of melt water,
+          ! accumulation of dirt and amount of new snow.
+          tmp = SnowFlag_3L(i) * SnowTemp(i) + ( 1 - SnowFlag_3L(i) ) * SoilTemp(i)
+          tmp = MIN( tmp, CTFRZ )
+          ar1(i) = 5000. * (1. / (CTFRZ-0.01) - 1. / tmp) ! crystal growth  (-ve)
+          ar2(i) = 10. * ar1(i) ! freezing of melt water
+
+          IF (soil_type(i) == perm_ice) THEN
+             
+             ! permanent ice: hard-wired number to be removed in future version
+             ar3(i) = .0000001
+             !  NB. dsnow =1,assumes pristine snow; ignores soot etc. ALTERNATIVELY,
+             !dnsnow = max (dnsnow, .5) !increase refreshing of snow in Antarctic
+             dnsnow(i) = 1.0
+
+          ELSE
+             ! accumulation of dirt
+             ar3(i) = .1
+
+          END IF
+       END IF
+    END DO
+
+    dtau = 1.e-6 * (EXP( ar1 ) + EXP( ar2 ) + ar3 ) * dels
+
+    WHERE (SnowDepth <= snow_depth_thresh)
+       SnowAge = 0.
+    ELSEWHERE
+       SnowAge = MAX(0.,(SnowAge+dtau)*(1.-dnsnow))
+    END WHERE
+
+    RETURN
+
+  END SUBROUTINE snow_aging
+
+END MODULE snow_aging_mod
