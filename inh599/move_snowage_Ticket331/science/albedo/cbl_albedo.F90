10da35cf8e5b8151b19ff559e999035c5506d49c
Ian Harman <inh599@nci.org.au>
2022-04-11 04:59:34 +0000

    general clean up including argument lists

--- inh599/move_snowage_Ticket331/science/albedo/cbl_albedo.F90
+++ inh599/move_snowage_Ticket331/science/albedo/cbl_albedo.F90
@@ -63,20 +63,20 @@ REAL :: AlbSnow(mp,nrb)             !Ground Albedo given a snow coverage (ssnow%
 REAL :: RadAlbedo(mp,nrb)           !Total albedo given RadFbeam (rad%albedo)
 
 !Forcing
-REAL :: MetTk(mp)                   !Air Temperture at surface - atmospheric forcing (met%tk)
+REAL :: MetTk(mp)                   !Air Temperture at surface - atmospheric forcing (met%tk) - not needed
 REAL :: coszen(mp)                  !cosine zenith angle  (met%coszen)
 REAL :: metDoY(mp)                  !Day of the Year - not always available (met%doy)
 REAL :: SW_down(mp,nrb)             !Downward shortwave "forced" (met%fsd)
 
 !Prognostics
 REAL :: SnowDepth(mp)               !Total Snow depth - water eqivalent - packed from snow_surft (SnowDepth)
-REAL :: SnowODepth(mp)              !Total Snow depth before any update this timestep (SnowODepth)
+REAL :: SnowODepth(mp)              !Total Snow depth before any update this timestep (SnowODepth)    - not needed
 REAL :: SnowDensity(mp)             !Total Snow density (assumes 1 layer describes snow cover) (SnowDensity)
 REAL :: SoilTemp(mp)                !Soil Temperature of top layer (soil%tgg)
-REAL :: SnowTemp(mp)                !Soil Temperature of top layer (soil%tgg)
+REAL :: SnowTemp(mp)                !Soil Temperature of top layer (soil%tgg) - not needed
 REAL :: SnowAge(mp)                 !Snow age (assumes 1 layer describes snow cover) (SnowAge)
 integer:: SnowFlag_3L(mp)           !Flag to treat snow as 3 layer - if enough present 
-                                    !Updated depending on total depth (SnowFlag_3L)
+                                    !Updated depending on total depth (SnowFlag_3L) - not needed
 
 REAL :: RadFbeam(mp,nrb)            !Computed Beam Fraction given total SW (rad%fbeam)
 
@@ -115,10 +115,12 @@ CanopyRefl_dif(:,:) = 0.0
 !CanopyTransmit_dif(:,:) = 0.0  ! MPI (at least inits this = 1.0 at dt=0) 
 
 !Modify parametrised soil albedo based on snow coverage 
-call surface_albedosn( AlbSnow, AlbSoil, mp, nrb, jls_radiation, surface_type, soil_type, &
-                       SnowDepth, SnowODepth, SnowFlag_3L,                      & 
-                       SnowDensity, SoilTemp, SnowTemp, SnowAge,                     & 
-                       MetTk, Coszen )
+!call surface_albedosn( AlbSnow, AlbSoil, mp, nrb, jls_radiation, surface_type, soil_type, &
+!                       SnowDepth, SnowODepth, SnowFlag_3L,                      & 
+!                       SnowDensity, SoilTemp, SnowTemp, SnowAge,                     & 
+!                       MetTk, Coszen )
+call surface_albedosn( AlbSnow, AlbSoil, mp, nrb, surface_type, soil_type, &
+                       SnowDepth, SnowDensity, SoilTemp, SnowAge, Coszen )
 
 ! Update fractional leaf transmittance and reflection
 !---1 = visible, 2 = nir radiaition
--- inh599/move_snowage_Ticket331/science/albedo/cbl_snow_albedo.F90
+++ inh599/move_snowage_Ticket331/science/albedo/cbl_snow_albedo.F90
@@ -7,53 +7,36 @@ MODULE cbl_snow_albedo_module
 
 CONTAINS
 
-  SUBROUTINE surface_albedosn( AlbSnow, AlbSoil, mp, nrb, jls_radiation, surface_type, soil_type, &
-                            SnowDepth, SnowODepth, SnowFlag_3L, SnowDensity, &
-                            SoilTemp, SnowTemp, SnowAge, & 
-                            metTk, coszen )
-    !H!jhan:Eliminate these USE data statements  
-    !USE cable_common_module, ONLY : kwidth_gl     !not needed anymore
-    !USE cable_phys_constants_mod, ONLY : CTFRZ => TFRZ  !not needed anymore
+  SUBROUTINE surface_albedosn( AlbSnow, AlbSoil, mp, nrb, surface_type, soil_type, &
+                            SnowDepth, SnowDensity, SoilTemp, SnowAge, coszen )
 
     IMPLICIT NONE
 
     !re-decl input args
     INTEGER, INTENT(IN) :: mp
     INTEGER, INTENT(IN) :: nrb
-    LOGICAL, INTENT(IN) :: jls_radiation             !runtime switch def. in cable_*main routines - not needed anymore
     REAL, INTENT(OUT)   :: AlbSnow(mp,nrb) 
     REAL, INTENT(IN)    :: AlbSoil(mp,nrb)           !NB to become IN OUT because of soil colour parameterization
-    REAL, INTENT(IN)    :: MetTk(mp)                 !not needed anymore
     REAL, INTENT(IN)    :: coszen(mp) 
     REAL, INTENT(IN)    :: SnowDepth(mp)
-    REAL, INTENT(IN)    :: SnowODepth(mp)            !not needed anymore
     REAL, INTENT(IN)    :: SnowDensity(mp)
     REAL, INTENT(IN)    :: SoilTemp(mp)                
-    REAL, INTENT(IN)    :: SnowTemp(mp)              !not needed anymore
     REAL, INTENT(IN)    :: SnowAge(mp)
-    INTEGER, INTENT(IN) :: SnowFlag_3L(mp)           !not needed anymore
     INTEGER, INTENT(IN) :: surface_type(mp)          
     INTEGER, INTENT(IN) :: soil_type(mp) 
 
     !working variables  
-    REAL, DIMENSION(mp) ::                                                      &
+    REAL, DIMENSION(mp) ::                                                         &
          alv,     &  ! Snow albedo for visible
          alir,    &  ! Snow albedo for near infra-red
-         !ar1,     &  ! crystal growth  (-ve)            !not needed anymore
-         !ar2,     &  ! freezing of melt water           !not needed anymore
-         !ar3,     &  !                                  !not needed anymore
-         !dnsnow,  &  ! new snow albedo                  !not needed anymore
-         !dtau,    &  !                                  !not needed anymore
          fage,    &  ! age factor
-         fzenm,   &  !
-         sfact,   &  !
-         snr,     &  !
-         snrat,   &  !
-         !talb,    &  ! snow albedo                      !legacy - not used
+         fzenm,   &  ! zenith factor
+         sfact,   &  ! soil factor
+         snrat,   &  ! (1-) fraction of soil 'seen' when evaluating surface albedo
          tmp,     &  ! temporary value
          SoilAlbsoilF
 
-    REAL, PARAMETER ::                                                          &
+    REAL, PARAMETER ::                                                             &
          alvo  = 0.95,  &  ! albedo for vis. on a new snow
          aliro = 0.70      ! albedo for near-infr. on a new snow
 
@@ -83,7 +66,7 @@ CONTAINS
        sfact = 0.62
     END WHERE
 
-    !first estimate of snow-affected albedos
+    !first estimate of snow-affected surface albedos
     AlbSnow(:,2) = 2.0 * SoilAlbsoilF / (1.0 + sfact)
     AlbSnow(:,1) = sfact * AlbSnow(:,2)
 
@@ -97,53 +80,26 @@ CONTAINS
     alir(:) =0.0
     alv(:)  =0.0
 
-    !Ticket 331 - snow aging evaluation moved to soilsnow routine
-    !NB since soil_type==perm_ice is overwritten at the end
-    !these conditions/that code can be removed; 
-    !the outer DO-IF can then be collapsed back to a WHERE loop
-    ! with the remaining IF replaced with a MERGE function
-    !for fzenm as per the original code
-    DO i = 1,mp
-       IF (SnowDepth(i) > snow_depth_thresh) THEN
+    !Ticket 331 - snow age evaluation moved to soilsnow routine
+    !removed permanent ice special conditions as overwritten later
+    WHERE (SnowDepth > snow_depth_thresh)
 
-          snr(i) = SnowDepth(i) / MAX (SnowDensity(i), 200.0)
+       !snrat is how little (as fraction) of the underlying soil 'seen'
+       tmp = SnowDepth / MAX (SnowDensity, 200.0)
+       snrat = MIN(1.0, tmp/ (tmp + 0.1))
 
-          IF (soil_type(i) == perm_ice) THEN
-             snrat(i) = 1.0
-          ELSE
-             snrat(i) = MIN (1.0, snr(i) / (snr(i) + 0.1) )
-          END IF
-        
-          !snow age and zenith angle factors
-          fage(i) = 1.0 - 1.0 / (1.0 + SnowAge(i) ) !age factor
-          tmp(i) = MAX (0.17365, coszen(i) )
-          !Ticket 331 - originally as MAX(0.0,MERGE(0.0,function,tmp>0.5))
-          IF (tmp(i)>0.5) THEN
-             fzenm(i) = 0.0
-          ELSE
-             !Ticket 331 - why isn't this written as
-             !fzenm(i) = 1.5/(1.0+4.0*tmp(i)) - 0.5
-             fzenm(i) = ( 1.0 + 1.0/2.0 ) / ( 1.0 + 2.0* 2.0 * tmp(i) ) - 1.0/2.0
-          END IF
-          fzenm(i) = MAX( 0.0, fzenm(i))
-           
-          
-          tmp(i) = alvo * (1.0 - 0.2 * fage(i))
-          alv(i) = 0.4 * fzenm(i) * (1.0 - tmp(i)) + tmp(i)
-          tmp(i) = aliro * (1.0 - 0.5 * fage(i))
+       !snow age and zenith angle factors
+       fage = 1.0 - 1.0 / (1.0 + SnowAge )
+       tmp = MAX (0.17365, coszen )
+       fzenm = MAX(0.0, MERGE(0.0, 1.5/(1.0+4.0*tmp) - 0.5,tmp>0.5) )
 
-          ! use dry snow albedo on permanent ice points
-          IF (soil_type(i) == perm_ice) THEN
-             tmp(i) = 0.95 * (1.0 - 0.2 * fage(i))
-             alv(i) = 0.4 * fzenm(i) * (1.0 - tmp(i)) + tmp(i)
-             tmp(i) = 0.75 * (1.0 - 0.5 * fage(i))
-          END IF
+       !alv and alir: aged-snow albedo
+       tmp = alvo * (1.0 - 0.2 * fage)
+       alv = 0.4 * fzenm * (1.0 - tmp) + tmp
+       tmp = aliro * (1.0 - 0.5 * fage )
+       alir = 0.4 * fzenm * (1.0 - tmp) + tmp
 
-          alir(i) = 0.4 * fzenm(i) * (1.0 - tmp(i)) + tmp(i)
-          !talb(i) = 0.5 * (alv(i) + alir(i)) ! snow albedo - not needed as not used!!
-
-       END IF
-    END DO        ! snowd > snow_deth_thresh
+    END WHERE
     
     !H!jhan:SLI currently not available
     !H!IF(cable_user%SOIL_STRUC=='sli') THEN
