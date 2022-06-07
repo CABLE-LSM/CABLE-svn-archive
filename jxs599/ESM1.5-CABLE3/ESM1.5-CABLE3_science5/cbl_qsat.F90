f18d36a75aae0ff4ddd050d47b43a8234234bac7
Jhan Srbinovsky <jxs599@nci.org.au>
2022-06-07 11:03:44 +0000

    as svnserv is down move back to SVN_CABLE - copy from earlier branch - binp *draft6 that works for all CALE reconciled except cable_canopy. Following #333 separate canopy module into independent modules. Thus far hae done this by replacing acble_canopy w trunk version HOWEVER NOT bitwise same - tried   again w ESM15 - but had to make so many changes to plumbing - NOT bitwise - so now break p canopy() inside ESM job - start with validated draft6 version - WORKS - out_FullCompile_draft6_canopy_ESM15_3_SVN_CABLE

---
+++ jxs599/ESM1.5-CABLE3/ESM1.5-CABLE3_science5/cbl_qsat.F90
@@ -0,0 +1,54 @@
+MODULE cbl_qsat_module
+
+PUBLIC qsatfjh
+PUBLIC qsatfjh2
+
+CONTAINS
+
+!E!SUBROUTINE qsatfjh(var,tair,pmb) 
+!==============================================================================
+SUBROUTINE qsatfjh(mp, var, CRMH2o, Crmair, CTETENA, CTETENB, CTETENC, tair,pmb)
+IMPLICIT NONE
+
+INTEGER, INTENT(IN) :: mp
+REAL, INTENT(IN) :: CRMH2O, CRMAIR
+REAL, INTENT(IN) :: CTETENA, CTETENB, CTETENC
+REAL, INTENT(IN) :: tair(mp)            ! air temperature (C)
+REAL, INTENT(IN) :: pmb(mp)             ! pressure PMB (mb)
+   
+REAL, INTENT(OUT) :: var(mp)             ! result; sat sp humidity
+
+!local vars
+INTEGER :: j
+  
+DO j=1,mp
+
+  var(j) = (CRMH2o/Crmair) * (CTETENA*EXP(CTETENB*tair(j) /(CTETENC+tair(j))))    &
+            / pmb(j)
+ENDDO   
+
+RETURN
+END SUBROUTINE qsatfjh
+!==============================================================================
+
+
+
+!E!SUBROUTINE qsatfjh2(var,tair,pmb) 
+!==============================================================================
+SUBROUTINE qsatfjh2( var, CRMH2o, Crmair, CTETENA, CTETENB, CTETENC, tair,pmb)
+IMPLICIT NONE
+
+REAL, INTENT(IN) :: CRMH2O, CRMAIR
+REAL, INTENT(IN) :: CTETENA, CTETENB, CTETENC
+REAL, INTENT(IN) :: tair            ! air temperature (C)
+REAL, INTENT(IN) :: pmb             ! pressure PMB (mb)
+   
+REAL, INTENT(OUT) :: var             ! result; sat sp humidity
+      
+var = (CRMH2o/Crmair) * (CTETENA*EXP(CTETENB*tair/(CTETENC+tair))) / pmb
+
+RETURN
+END SUBROUTINE qsatfjh2
+!==============================================================================
+
+END MODULE cbl_qsat_module
