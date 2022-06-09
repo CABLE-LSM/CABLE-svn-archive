15027817949f05138388b30ec40044d838af85dc
Jhan Srbinovsky <jxs599@nci.org.au>
2022-06-09 12:54:32 +0000

    out_PartCompile_draft6_canopy_ESM15_8895_1

---
+++ jxs599/ESM1.5-CABLE3/ESM1.5-CABLE3_science5/cbl_zetar.F90
@@ -0,0 +1,71 @@
+MODULE cbl_zetar_module
+
+IMPLICIT NONE
+
+PUBLIC update_zetar
+
+PRIVATE
+
+CONTAINS
+
+SUBROUTINE update_zetar( mp, NITER, canopy_zetar, iter, nrb, CVONK, CGRAV, CCAPP,  &
+                   CLAI_THRESH, CZETmul, CZETPOS, CZETNEG,          &
+                   cable_user_soil_struc, air_rho, met_tk,  met_fsd, &
+                   rough_zref_tq, rough_hruff, rough_term6a, rough_z0soilsn,   &
+                   canopy_vlaiw, canopy_zetash,  canopy_us, &
+                   canopy_fh, canopy_fe, canopy_fhs, canopy_fes ) 
+IMPLICIT NONE
+
+INTEGER, INTENT(IN) :: mp
+INTEGER, INTENT(IN) :: NITER
+INTEGER, INTENT(IN) :: nrb
+
+REAL, INTENT(OUT) :: canopy_zetar(mp, NITER)
+REAL, INTENT(OUT) :: canopy_zetash(mp, NITER)
+INTEGER, INTENT(IN) :: iter
+
+! constants
+REAL, INTENT(IN) :: CVONK, CGRAV, CCAPP, CLAI_THRESH, CZETmul, CZETPOS, CZETNEG
+CHARACTER, INTENT(IN)  :: cable_user_soil_struc
+
+REAL, INTENT(IN) :: air_rho(mp)
+REAL, INTENT(IN) :: met_tk(mp)
+REAL, INTENT(IN) :: met_fsd(mp,nrb)
+REAL, INTENT(IN) :: rough_zref_tq(mp)
+REAL, INTENT(IN) :: rough_hruff(mp)
+REAL, INTENT(IN) :: rough_term6a(mp)
+REAL, INTENT(IN) :: rough_z0soilsn(mp)
+REAL, INTENT(IN) :: canopy_vlaiw(mp)
+REAL, INTENT(IN) :: canopy_us(mp)
+REAL, INTENT(IN) :: canopy_fh(mp)
+REAL, INTENT(IN) :: canopy_fe(mp)
+REAL, INTENT(IN) :: canopy_fhs(mp)
+REAL, INTENT(IN) :: canopy_fes(mp)
+
+!local vars
+INTEGER :: iterplus
+INTEGER :: j
+
+! monin-obukhov stability parameter zetar=zref/l
+! recompute zetar for the next iteration, except on last iteration
+IF (iter < NITER) THEN ! dont compute zetar on the last iter
+
+  iterplus = MAX(iter+1,2)
+  canopy_zetar(:,iterplus) = -( CVONK * CGRAV * rough_zref_tq *              &
+                             ( canopy_fh + 0.07 * canopy_fe ) ) /          &
+                             ( air_rho * CCAPP * met_tk * canopy_us**3 )
+
+  ! constrain zeta to CZETPOS and CZETNEG (set in param0)
+  
+  ! zetar too +
+  canopy_zetar(:,iterplus) = MIN(CZETPOS,canopy_zetar(:,iterplus))        
+  
+  ! zetar too -
+  canopy_zetar(:,iterplus) = MAX(CZETNEG,canopy_zetar(:,iterplus))        
+
+END IF ! (iter < NITER)
+
+RETURN
+END SUBROUTINE update_zetar
+
+END MODULE cbl_zetar_module
