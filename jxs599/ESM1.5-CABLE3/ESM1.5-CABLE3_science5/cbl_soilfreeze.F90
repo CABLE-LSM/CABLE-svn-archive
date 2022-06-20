2ddab7d4bf3fce9e685bd0024f7a2db2db5c9e76
Jhan Srbinovsky <jxs599@nci.org.au>
2022-06-20 12:38:59 +0000

    trivial in main AND accomodate r_2 issue causing diff in trunk

--- jxs599/ESM1.5-CABLE3/ESM1.5-CABLE3_science5/cbl_soilfreeze.F90
+++ jxs599/ESM1.5-CABLE3/ESM1.5-CABLE3_science5/cbl_soilfreeze.F90
@@ -17,7 +17,8 @@ IMPLICIT NONE
     REAL, DIMENSION(mp)           :: xx
 INTEGER :: i,k
 REAL :: heat_cap_lower_limit(mp,ms)
-REAL :: max_arg1, max_arg2
+REAL :: max_arg1
+REAL(r_2) :: max_arg2
  
 xx = 0.
 DO k = 1, ms  !loop over soil levels
@@ -32,14 +33,14 @@ DO k = 1, ms  !loop over soil levels
       ssnow%wbice(i,k) = MIN( ssnow%wbice(i,k) + sicefreeze(i) / (soil%zse(k)  &
                          * 1000.0), frozen_limit * ssnow%wb(i,k) )
       xx(i) = soil%css(i) * soil%rhosoil(i)
-      max_arg1 = REAL((1.0 - soil%ssat(i)) * soil%css(i) * soil%rhosoil(i) ,r_2) &
+      IF( cable_runtime%esm15 ) THEN
+        max_arg1 = xx(i) 
+      ELSE  
+        max_arg1 = heat_cap_lower_limit(i,k) 
+      ENDIF
+      max_arg2 = REAL((1.0 - soil%ssat(i)) * soil%css(i) * soil%rhosoil(i) ,r_2) &
           + (ssnow%wb(i,k) - ssnow%wbice(i,k)) * REAL(Ccswat * Cdensity_liq,r_2)  &
           + ssnow%wbice(i,k) * REAL(Ccsice * Cdensity_liq * 0.9,r_2)
-      IF( cable_runtime%esm15 ) THEN
-        max_arg2 = xx(i) 
-      ELSE  
-        max_arg2 = heat_cap_lower_limit(i,k) 
-      ENDIF
       ssnow%gammzz(i,k) = MAX( max_arg1, max_arg2 ) * REAL( soil%zse(k),r_2 )
 
       IF (k == 1 .AND. ssnow%isflag(i) == 0)  &
@@ -56,14 +57,14 @@ DO k = 1, ms  !loop over soil levels
       ssnow%wbice(i,k) = MAX( 0.0_r_2, ssnow%wbice(i,k) - sicemelt(i)          &
                          / (soil%zse(k) * 1000.0) )
       xx(i) = soil%css(i) * soil%rhosoil(i)
-      max_arg1 = REAL((1.0 - soil%ssat(i)) * soil%css(i) * soil%rhosoil(i) ,r_2) &
+      IF( cable_runtime%esm15 ) THEN
+        max_arg1 = xx(i) 
+      ELSE  
+        max_arg1 = heat_cap_lower_limit(i,k) 
+      ENDIF
+      max_arg2 = REAL((1.0 - soil%ssat(i)) * soil%css(i) * soil%rhosoil(i) ,r_2) &
           + (ssnow%wb(i,k) - ssnow%wbice(i,k)) * REAL(Ccswat * Cdensity_liq,r_2)  &
           + ssnow%wbice(i,k) * REAL(Ccsice * Cdensity_liq * 0.9,r_2)
-      IF( cable_runtime%esm15 ) THEN
-        max_arg2 = xx(i) 
-      ELSE  
-        max_arg2 = heat_cap_lower_limit(i,k) 
-      ENDIF
       
       ssnow%gammzz(i,k) = MAX( max_arg1, max_arg2 ) * REAL( soil%zse(k),r_2 )
 
