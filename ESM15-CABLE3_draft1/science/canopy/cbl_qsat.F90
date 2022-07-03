c0f0d189ac5a098718bd35174ef437c195fcac86
Jhan Srbinovsky <jxs599@nci.org.au>
2022-07-03 08:45:16 +0000

    w these 3 works - reverted friction_vel - i think I know why

--- ESM15-CABLE3_draft1/science/canopy/cbl_qsat.F90
+++ ESM15-CABLE3_draft1/science/canopy/cbl_qsat.F90
@@ -5,40 +5,48 @@ PUBLIC qsatfjh2
 
 CONTAINS
 
+!==============================================================================
+
 SUBROUTINE qsatfjh(mp, var, CRMH2o, Crmair, CTETENA, CTETENB, CTETENC, tair,pmb)
+IMPLICIT NONE
 
-IMPLICIT  NONE   
-integer :: mp 
-REAL, INTENT(OUT) :: var(mp)         ! result; sat sp humidity
-REAL :: CRMH2o, Crmair, CTETENA, CTETENB, CTETENC
-REAL, INTENT(IN) ::                                          &
-  tair(mp),                        & ! air temperature (C)
-  pmb(mp)                            ! pressure PMB (mb)
+INTEGER, INTENT(IN) :: mp
+REAL, INTENT(IN) :: CRMH2O, CRMAIR
+REAL, INTENT(IN) :: CTETENA, CTETENB, CTETENC
+REAL, INTENT(IN) :: tair(mp)            ! air temperature (C)
+REAL, INTENT(IN) :: pmb(mp)             ! pressure PMB (mb)
+   
+REAL, INTENT(OUT) :: var(mp)             ! result; sat sp humidity
 
+!local vars
 INTEGER :: j
-
+  
 DO j=1,mp
 
-  var(j) = (CRMH2o/Crmair) * (CTETENA*EXP(CTETENB*tair(j)/(CTETENC+tair(j))))  &
-          / pmb(j)
-ENDDO
+  var(j) = (CRMH2o/Crmair) * (CTETENA*EXP(CTETENB*tair(j) /(CTETENC+tair(j))))    &
+            / pmb(j)
+ENDDO   
 
+RETURN
 END SUBROUTINE qsatfjh
 
-
+!==============================================================================
 
 SUBROUTINE qsatfjh2( var, CRMH2o, Crmair, CTETENA, CTETENB, CTETENC, tair,pmb)
+IMPLICIT NONE
 
-REAL :: CRMH2o, Crmair, CTETENA, CTETENB, CTETENC
-  REAL, INTENT(IN) ::                                                         &
-       tair,         & ! air temperature (C)
-       pmb             ! pressure PMB (mb)
+REAL, INTENT(IN) :: CRMH2O, CRMAIR
+REAL, INTENT(IN) :: CTETENA, CTETENB, CTETENC
+REAL, INTENT(IN) :: tair            ! air temperature (C)
+REAL, INTENT(IN) :: pmb             ! pressure PMB (mb)
+   
+REAL, INTENT(OUT) :: var             ! result; sat sp humidity
+      
+var = (CRMH2o/Crmair) * (CTETENA*EXP(CTETENB*tair/(CTETENC+tair))) / pmb
 
-  REAL, INTENT(OUT) ::                                                        &
-       var             ! result; sat sp humidity
-
-  var = (CRMH2o/Crmair) * (CTETENA*EXP(CTETENB*tair/(CTETENC+tair))) / pmb
-
+RETURN
 END SUBROUTINE qsatfjh2
+
+!==============================================================================
 
 END MODULE cbl_qsat_module
