36fb61ad36700e4aa8cb7d97a34d30ab6fbadbc7
Jhan Srbinovsky <jxs599@nci.org.au>
2022-07-19 23:17:54 +0000

    updated

--- jxs599/CABLE/Tickets/2022/338/params/cable_maths_constants_mod.F90
+++ jxs599/CABLE/Tickets/2022/338/params/cable_maths_constants_mod.F90
@@ -13,6 +13,6 @@ PUBLIC
 !-----------------------------------------------------------------------------
 
 REAL, PARAMETER :: pi    = 3.1415927
-REAL, PARAMETER :: pi180 = 3.1415927/ 180.0 ! radians / degree
+REAL, PARAMETER :: pi180 = pi / 180.0 ! radians / degree
 
 END MODULE cable_math_constants_mod
