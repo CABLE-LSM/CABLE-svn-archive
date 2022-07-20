ad227c102719d049fbeefa7d28a267e45dbe83fa
Jhan Srbinovsky <jxs599@nci.org.au>
2022-07-20 00:12:29 +0000

    umdp3 checker compliance

--- jxs599/CABLE/Tickets/2022/338/science/roughness/cbl_lai_eff.F90
+++ jxs599/CABLE/Tickets/2022/338/science/roughness/cbl_lai_eff.F90
@@ -1,33 +1,33 @@
 MODULE cbl_LAI_eff_mod
 
-   IMPLICIT NONE
+IMPLICIT NONE
 
-   PUBLIC LAI_eff 
+PUBLIC LAI_eff
 
 CONTAINS
 
 !Computes Effective LAI of exposed canopy given effect of snow present
 !variable formerly known as canopy%vlaiw
-SUBROUTINE LAI_eff( mp, LAI_PFT, Hgt_PFT, HgtAboveSnow,  &
-                    reducedLAIdue2snow ) 
+SUBROUTINE LAI_eff( mp, lai_pft, Hgt_PFT, HgtAboveSnow,                        &
+                    reducedLAIdue2snow )
 
-  !re-decl input args  
-  integer  :: mp
-  real :: LAI_PFT(mp)
-  real :: Hgt_PFT(mp)
-  real :: HgtAboveSnow(mp) 
-  real :: reducedLAIdue2snow(mp) 
- 
-  !local_vars: 
-  real :: Hgt_eff(mp)
-  real :: FracOfCanopyAboveSnow(mp)
+  !re-decl input args
+INTEGER  :: mp
+REAL :: lai_pft(mp)
+REAL :: Hgt_PFT(mp)
+REAL :: HgtAboveSnow(mp)
+REAL :: reducedLAIdue2snow(mp)
 
-  !Fraction Of Canopy Above Snow
-  FracOfCanopyAboveSnow = HgtAboveSnow/ MAX( 0.01, Hgt_PFT)
-  
-  ! LAI decreases due to snow:
-  reducedLAIdue2snow = LAI_PFT * FracOfCanopyAboveSnow 
+!local_vars:
+REAL :: Hgt_eff(mp)
+REAL :: FracOfCanopyAboveSnow(mp)
+
+!Fraction Of Canopy Above Snow
+FracOfCanopyAboveSnow = HgtAboveSnow/ MAX( 0.01, Hgt_PFT)
+
+! LAI decreases due to snow:
+reducedLAIdue2snow = lai_pft * FracOfCanopyAboveSnow
 
 END SUBROUTINE LAI_eff
 
-End MODULE cbl_LAI_eff_mod
+END MODULE cbl_LAI_eff_mod
