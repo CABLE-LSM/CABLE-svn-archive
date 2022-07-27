02c92ed08d70dde250183f236b39b6d907e3cd6a
Jhan Srbinovsky <jxs599@nci.org.au>
2022-07-27 11:26:39 +0000

    replace the two files with single module

--- jxs599/CABLE/Tickets/2022/338/science/roughness/roughHGT_effLAI_cbl.F90
+++ jxs599/CABLE/Tickets/2022/338/science/roughness/roughHGT_effLAI_cbl.F90
@@ -1,10 +1,12 @@
-MODULE cbl_hruff_mod
+MODULE eff_LAI_mod_cbl
 
 IMPLICIT NONE
 
 PUBLIC HgtAboveSnow
+PUBLIC LAI_eff
 
 CONTAINS
+
 !Computes height above surface given that therre is snow present
 !variable formerly known as rough%hruff
 SUBROUTINE HgtAboveSnow( HeightAboveSnow, mp, z0surf_min, HGT_pft,             &
@@ -47,4 +49,28 @@ RETURN
 
 END SUBROUTINE HgtAboveSnow
 
-END MODULE cbl_hruff_mod
+!Computes Effective LAI of exposed canopy given effect of snow present
+!variable formerly known as canopy%vlaiw
+SUBROUTINE LAI_eff( mp, lai_pft, Hgt_PFT, HgtAboveSnow,                        &
+                    reducedLAIdue2snow )
+
+  !re-decl input args
+INTEGER  :: mp
+REAL :: lai_pft(mp)
+REAL :: Hgt_PFT(mp)
+REAL :: HgtAboveSnow(mp)
+REAL :: reducedLAIdue2snow(mp)
+
+!local_vars:
+REAL :: Hgt_eff(mp)
+REAL :: FracOfCanopyAboveSnow(mp)
+
+!Fraction Of Canopy Above Snow
+FracOfCanopyAboveSnow = HgtAboveSnow/ MAX( 0.01, Hgt_PFT)
+
+! LAI decreases due to snow:
+reducedLAIdue2snow = lai_pft * FracOfCanopyAboveSnow
+
+END SUBROUTINE LAI_eff
+
+END MODULE eff_LAI_mod_cbl
