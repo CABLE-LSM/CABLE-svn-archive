836d850924931877042b6852a20f058a1e6bb8e3
Jhan Srbinovsky <jxs599@nci.org.au>
2022-09-07 01:37:15 +0000

    ci these differences tahzt appear to be trivial formatting only

--- jxs599/CABLE/Tickets/2022/338/science/radiation/cbl_rhoch.F90
+++ jxs599/CABLE/Tickets/2022/338/science/radiation/cbl_rhoch.F90
@@ -2,7 +2,7 @@ MODULE cbl_rhoch_module
 
 IMPLICIT NONE
 
-PUBLIC calc_rhoch
+PUBLIC :: calc_rhoch
 PRIVATE
 
 CONTAINS
@@ -10,12 +10,16 @@ CONTAINS
 ! this subroutine called from _init_radiation on cable_albedo.F90 pathway, explicit and implict
 SUBROUTINE calc_rhoch( c1,rhoch, mp, nrb, taul, refl )
 
-INTEGER :: mp
-INTEGER :: nrb
-REAL :: c1(mp,nrb)
-REAL :: rhoch(mp,nrb)
-REAL :: taul(mp,nrb)
-REAL :: refl(mp,nrb)
+IMPLICIT NONE
+
+!model dimensions
+INTEGER, INTENT(IN) :: mp                     ! number of "tiles"
+INTEGER, INTENT(IN) :: nrb                    ! # of rad. bands VIS,NIR(,LW)
+
+REAL, INTENT(OUT) :: c1(mp,nrb)
+REAL, INTENT(OUT) :: rhoch(mp,nrb)            ! REFLection black horiz leaves
+REAL, INTENT(IN) :: taul(mp,nrb)              ! Leaf Transmisivity
+REAL, INTENT(IN) :: refl(mp,nrb)              ! Leaf Reflectivity
 
 c1(:,1) = SQRT(1.0 - taul(:,1) - refl(:,1))
 c1(:,2) = SQRT(1.0 - taul(:,2) - refl(:,2))
@@ -25,6 +29,7 @@ c1(:,3) = 1.0
 ! (eq. 6.19 in Goudriaan and van Laar, 1994):
 rhoch = (1.0 - c1) / (1.0 + c1)
 
+RETURN
 END SUBROUTINE calc_rhoch
 
 END MODULE cbl_rhoch_module
--- jxs599/CABLE/Tickets/2022/338/science/radiation/cbl_spitter.F90
+++ jxs599/CABLE/Tickets/2022/338/science/radiation/cbl_spitter.F90
@@ -2,28 +2,26 @@ MODULE cbl_spitter_module
 
 IMPLICIT NONE
 
-PUBLIC spitter
+PUBLIC :: spitter
 PRIVATE
 
 CONTAINS
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-! Calculate beam fraction: See Spitters et al. 1986, agric. for meteorol., 38:217-229
+! Calc. beam fraction: Spitters et al. 1986, agric. for meteorol., 38:217-229
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 FUNCTION spitter(mp, cpi, doy, coszen, fsd) RESULT(fbeam)
 IMPLICIT NONE
-!re-decl input args
-INTEGER :: mp
-REAL :: cpi
-INTEGER :: doy(mp)       ! day of year !typecast from integer
-REAL :: coszen(mp)    ! cos(zenith angle of sun)
-REAL :: fsd(mp)       ! short wave down (positive) w/m^2
 
-REAL, DIMENSION(mp) ::                                                         &
-   fbeam,      & ! beam fraction (result)
-   tmpr,       & !
-   tmpk,       & !
-   tmprat        !
-
+INTEGER, INTENT(IN) :: mp         ! CABLE vector length of active tiles
+REAL :: fbeam(mp)                 ! beam fraction of total SW (f^n result)
+REAL, INTENT(IN) :: cpi           ! PI
+REAL, INTENT(IN) :: coszen(mp)    ! cos(zenith angle of sun)
+REAL, INTENT(IN) :: fsd(mp)       ! short wave down (positive) w/m^2
+INTEGER, INTENT(IN) :: doy(mp)    ! day of year !typecast from integer
+!local
+REAL :: tmpr(mp)
+REAL :: tmpk(mp)
+REAL :: tmprat(mp)
 REAL, PARAMETER :: solcon = 1370.0
 
 fbeam = 0.0
@@ -37,13 +35,17 @@ ELSE WHERE
   tmprat = 0.0
 END WHERE
 
-WHERE ( tmprat > 0.22 ) fbeam = 6.4 * ( tmprat - 0.22 )**2
+WHERE ( tmprat > 0.22 )
+  fbeam = 6.4 * ( tmprat - 0.22 )**2
+END WHERE
+WHERE ( tmprat > 0.35 )
+  fbeam = MIN( 1.66 * tmprat - 0.4728, 1.0 )
+END WHERE
+WHERE ( tmprat > tmpk )
+  fbeam = MAX( 1.0 - tmpr, 0.0 )
+END WHERE
 
-WHERE ( tmprat > 0.35 ) fbeam = MIN( 1.66 * tmprat - 0.4728, 1.0 )
-
-WHERE ( tmprat > tmpk ) fbeam = MAX( 1.0 - tmpr, 0.0 )
-
+RETURN
 END FUNCTION spitter
-
 
 END MODULE cbl_spitter_module
