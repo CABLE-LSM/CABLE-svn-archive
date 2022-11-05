d8a8cdf6698659bdb5c1edf0b45c38969310d473
Jhan Srbinovsky <jxs599@nci.org.au>
2022-11-05 11:44:25 +0000

    mods from ESM15 side

--- jxs599/CABLE/Tickets/2022/351/science/casa-cnp/biogeochem_casa.F90
+++ jxs599/CABLE/Tickets/2022/351/science/casa-cnp/biogeochem_casa.F90
@@ -1,27 +1,36 @@
-  SUBROUTINE biogeochem(ktau,dels,idoY,LALLOC,veg,soil,casabiome,casapool,casaflux, &
+
+#define ESM_BUILD YES
+
+MODULE biogeochem_mod
+
+IMPLICIT NONE
+
+CONTAINS
+
+SUBROUTINE biogeochem(ktau,dels,idoY,LALLOC,veg,soil,casabiome,casapool,casaflux, &
        casamet,casabal,phen,POP,climate,xnplimit,xkNlimiting,xklitter,xksoil,xkleaf,xkleafcold,xkleafdry,&
        cleaf2met,cleaf2str,croot2met,croot2str,cwood2cwd,         &
        nleaf2met,nleaf2str,nroot2met,nroot2str,nwood2cwd,         &
        pleaf2met,pleaf2str,proot2met,proot2str,pwood2cwd)
-    USE cable_def_types_mod
-    USE casadimension
-    USE casa_cnp_module
+  USE cable_def_types_mod
+  USE casadimension
+  USE casa_cnp_module
     USE POP_TYPES,            ONLY: POP_TYPE
-    IMPLICIT NONE
-    INTEGER, INTENT(IN)    :: ktau
-    REAL,    INTENT(IN)    :: dels
-    INTEGER, INTENT(IN)    :: idoy
-    INTEGER, INTENT(IN)    :: LALLOC
-    TYPE (veg_parameter_type),    INTENT(INOUT) :: veg  ! vegetation parameters
-    TYPE (soil_parameter_type),   INTENT(INOUT) :: soil ! soil parameters
-    TYPE (casa_biome),            INTENT(INOUT) :: casabiome
-    TYPE (casa_pool),             INTENT(INOUT) :: casapool
-    TYPE (casa_flux),             INTENT(INOUT) :: casaflux
-    TYPE (casa_met),              INTENT(INOUT) :: casamet
-    TYPE (casa_balance),          INTENT(INOUT) :: casabal
-    TYPE (phen_variable),         INTENT(INOUT) :: phen
-    TYPE(POP_TYPE),             INTENT(IN) :: POP
-    TYPE(climate_TYPE),             INTENT(IN) :: climate
+  IMPLICIT NONE
+  INTEGER, INTENT(IN)    :: ktau
+  REAL,    INTENT(IN)    :: dels
+  INTEGER, INTENT(IN)    :: idoy
+  INTEGER, INTENT(IN)    :: lalloc
+  TYPE (veg_parameter_type),    INTENT(INOUT) :: veg  ! vegetation parameters
+  TYPE (soil_parameter_type),   INTENT(INOUT) :: soil ! soil parameters  
+  TYPE (casa_biome),            INTENT(INOUT) :: casabiome
+  TYPE (casa_pool),             INTENT(INOUT) :: casapool
+  TYPE (casa_flux),             INTENT(INOUT) :: casaflux
+  TYPE (casa_met),              INTENT(INOUT) :: casamet
+  TYPE (casa_balance),          INTENT(INOUT) :: casabal
+  TYPE (phen_variable),         INTENT(INOUT) :: phen
+  TYPE(POP_TYPE),             INTENT(IN) :: POP
+ TYPE(climate_TYPE),             INTENT(IN) :: climate
 
     ! local variables added by ypwang following Chris Lu 5/nov/2012
 
@@ -29,11 +38,11 @@
          nleaf2met,nleaf2str,nroot2met,nroot2str,nwood2cwd,         &
          pleaf2met,pleaf2str,proot2met,proot2str,pwood2cwd
 
-    ! local variables
-    REAL(r_2),    DIMENSION(mp) :: xnplimit,xNPuptake
-    REAL(r_2),    DIMENSION(mp) :: xklitter,xksoil,xkNlimiting
-    REAL(r_2),    DIMENSION(mp) :: xkleafcold,xkleafdry,xkleaf
-    INTEGER  npt,j
+  ! local variables
+  REAL(r_2),    DIMENSION(mp) :: xnplimit,xNPuptake
+  REAL(r_2),    DIMENSION(mp) :: xklitter,xksoil,xkNlimiting
+  REAL(r_2),    DIMENSION(mp) :: xkleafcold,xkleafdry,xkleaf
+  INTEGER  npt,j
     REAL, ALLOCATABLE :: tmp(:)
     real latx,lonx
     integer ivegx,isox
@@ -41,32 +50,37 @@
     ! check P balance for a selected land point
     latx=30.00;lonx=-89.0625;ivegx=2;isox=6
 
-    xKNlimiting = 1.0
+  xKNlimiting = 1.0
 
     ! zero annual sums
     IF (idoy==1) CALL casa_cnpflux(casaflux,casapool,casabal,.TRUE.)
 
     IF (cable_user%PHENOLOGY_SWITCH.EQ.'MODIS') THEN
-       CALL phenology(idoy,veg,phen)
+      call phenology(idoy,veg,phen)
     ENDIF
-    CALL avgsoil(veg,soil,casamet)
-    CALL casa_rplant1(veg,casabiome,casapool,casaflux,casamet)
+    call avgsoil(veg,soil,casamet)
 
+#     ifdef ESM_BUILD 
+      call casa_rplant(veg,casabiome,casapool,casaflux,casamet,climate)
+#     else  
+      CALL casa_rplant1(veg,casabiome,casapool,casaflux,casamet)
+#     endif
 
     IF (.NOT.cable_user%CALL_POP) THEN
-       CALL casa_allocation(veg,soil,casabiome,casaflux,casapool,casamet,phen,LALLOC)
+      call casa_allocation(lalloc, veg,soil,casabiome,casaflux,casapool,casamet,phen)
     ENDIF
 
-    CALL casa_xrateplant(xkleafcold,xkleafdry,xkleaf,veg,casabiome, &
-         casamet,phen)
-    CALL casa_coeffplant(xkleafcold,xkleafdry,xkleaf,veg,casabiome,casapool, &
+  call casa_xrateplant(xkleafcold,xkleafdry,xkleaf,veg,casabiome, &
+                       casamet,phen)
+  call casa_coeffplant(xkleafcold,xkleafdry,xkleaf,veg,casabiome,casapool, &
          casaflux,casamet,phen)
 
-    CALL casa_xnp(xnplimit,xNPuptake,veg,casabiome,casapool,casaflux,casamet)
+  call casa_xnp(xnplimit,xNPuptake,veg,casabiome,casapool,casaflux,casamet)
 
+#ifndef ESM_BUILD !possibly dont need as excludedd by IF CALL_POP
     IF (cable_user%CALL_POP) THEN
 
-       CALL casa_allocation(veg,soil,casabiome,casaflux,casapool,casamet,phen,LALLOC)
+      call casa_allocation(lalloc, veg,soil,casabiome,casaflux,casapool,casamet,phen)
        WHERE (pop%pop_grid(:)%cmass_sum_old.GT.0.001 .AND. pop%pop_grid(:)%cmass_sum.GT.0.001 )
 
           casaflux%frac_sapwood(POP%Iwood) = POP%pop_grid(:)%csapwood_sum/ POP%pop_grid(:)%cmass_sum
@@ -100,54 +114,41 @@
        ENDWHERE
 
     ENDIF
-!!$if (idoy.eq.365) then
-!!$ write(667,*) pop%LU
-!!$ write(667,*) veg%ilu
-!!$ write(667,991) casaflux%FluxCtohwp(POP%Iwood,1)
-!!$ write(667,991) POP%pop_grid(:)%cat_mortality/POP%pop_grid(:)%cmass_sum_old
-!!$   write(667,991)max(min((POP%pop_grid(:)%cat_mortality                &
-!!$        /POP%pop_grid(:)%cmass_sum_old),0.99),0.0)**(1.0/365.0)
-!!$   write(667,991) (1.0 - (1.0 -max( min((POP%pop_grid(:)%cat_mortality  &
-!!$        /POP%pop_grid(:)%cmass_sum_old),0.99), 0.0))**(1.0/365.0))
-!!$write(667,*)
-!!$   endif
-!!$
-    !write(667,991) casaflux%cgpp(147),casaflux%cnpp(147),casaflux%kplant(147,2),casapool%cplant(147,:)
-    !  write(*,991)casaflux%cgpp(2058),casaflux%cnpp(2058),casaflux%fracClabile(2058), &
-    !            casaflux%fracCalloc(2058,:),casaflux%crmplant(2058,:),casaflux%crgplant(2058), casapool%Nsoilmin(2058), &
-    !            casaflux%cgpp(2058)-casaflux%cnpp(2058)-casaflux%fracClabile(2058)*casaflux%cgpp(2058)-sum(casaflux%crmplant(2058,:))-casaflux%crgplant(2058)
-    !991  format('point 147',20(f10.4,2x))
-991 FORMAT(20(e12.4,2x))
 
-    CALL casa_xratesoil(xklitter,xksoil,veg,soil,casamet,casabiome)
-    CALL casa_coeffsoil(xklitter,xksoil,veg,soil,casabiome,casaflux,casamet)
 
-    IF (icycle>1) THEN
-       CALL casa_xkN(xkNlimiting,casapool,casaflux,casamet,casabiome,veg)
-       DO j=1,mlitter
-          casaflux%klitter(:,j) = casaflux%klitter(:,j)* xkNlimiting(:)
-       ENDDO
-       CALL casa_nuptake(veg,xkNlimiting,casabiome,casapool,casaflux,casamet)
-       IF (icycle >2) CALL casa_puptake(veg,xkNlimiting,casabiome, &
-            casapool,casaflux,casamet)
-    ENDIF
 
-    ! changed by ypwang following Chris Lu on 5/nov/2012
+#   endif
+  call casa_xratesoil(xklitter,xksoil,veg,soil,casamet,casabiome)
+  call casa_coeffsoil(xklitter,xksoil,veg,soil,casabiome,casaflux,casamet)
+
+  IF (icycle>1) THEN
+    call casa_xkN(xkNlimiting,casapool,casaflux,casamet,casabiome,veg)
+    DO j=1,mlitter
+      casaflux%klitter(:,j) = casaflux%klitter(:,j)* xkNlimiting(:)
+    ENDDO
+    call casa_nuptake(veg,xkNlimiting,casabiome,casapool,casaflux,casamet)
+    IF (icycle >2) call casa_puptake(veg,xkNlimiting,casabiome, &
+                                     casapool,casaflux,casamet)
+  ENDIF 
+
     CALL casa_delplant(veg,casabiome,casapool,casaflux,casamet,                &
          cleaf2met,cleaf2str,croot2met,croot2str,cwood2cwd,  &
          nleaf2met,nleaf2str,nroot2met,nroot2str,nwood2cwd,  &
          pleaf2met,pleaf2str,proot2met,proot2str,pwood2cwd)
+      
+      casaflux%Cplant_turnover_disturbance = 0
+      casaflux%Cplant_turnover_crowding = 0
+      casaflux%Cplant_turnover_resource_limitation = 0
 
-    casaflux%Cplant_turnover_disturbance = 0
-    casaflux%Cplant_turnover_crowding = 0
-    casaflux%Cplant_turnover_resource_limitation = 0
-
-    IF (cable_user%CALL_POP) THEN
-       IF (.NOT.ALLOCATED(tmp)) ALLOCATE(tmp(SIZE(POP%pop_grid)))
-       tmp = (POP%pop_grid(:)%stress_mortality + POP%pop_grid(:)%crowding_mortality &
+      IF (cable_user%CALL_POP) THEN
+        
+        IF (.NOT.ALLOCATED(tmp)) ALLOCATE(tmp(SIZE(POP%pop_grid)))
+        
+        tmp = (POP%pop_grid(:)%stress_mortality + POP%pop_grid(:)%crowding_mortality &
             +POP%pop_grid(:)%cat_mortality &
             + POP%pop_grid(:)%fire_mortality  )
-       WHERE (tmp.GT. 1.e-12)
+        
+        WHERE (tmp.GT. 1.e-12)
           casaflux%Cplant_turnover_disturbance(POP%Iwood) =  &
                casaflux%Cplant_turnover(POP%Iwood,2)*(POP%pop_grid(:)%cat_mortality &
                + POP%pop_grid(:)%fire_mortality  )/tmp
@@ -155,22 +156,22 @@
                casaflux%Cplant_turnover(POP%Iwood,2)*POP%pop_grid(:)%crowding_mortality/tmp
           casaflux%Cplant_turnover_resource_limitation(POP%Iwood) = &
                casaflux%Cplant_turnover(POP%Iwood,2)*POP%pop_grid(:)%stress_mortality/tmp
-       endwhere
-    ENDIF
+        ENDWHERE
 
-    CALL casa_delsoil(veg,casapool,casaflux,casamet,casabiome)
+      ENDIF !IF (cable_user%CALL_POP) THEN
 
-    ! add "casabal" to store pool sizes before updating
-    CALL casa_cnpcycle(veg,casabiome,casapool,casaflux,casamet,casabal, LALLOC)
+  call casa_delsoil(veg,casapool,casaflux,casamet,casabiome)
 
-    !! vh_js !!
+  ! add "casabal" to store pool sizes before updating
+  call casa_cnpcycle(veg,casabiome,casapool,casaflux,casamet,casabal,LALLOC)
+
     !CLN ndummy must be before pdummy!!!!
-    IF (icycle<3) THEN
-        IF (icycle<2) CALL casa_ndummy(casamet,casabal,casapool)
+  IF (icycle<3) then
+      IF (icycle<2) call casa_ndummy(casamet,casabal,casapool)
         CALL casa_pdummy(casamet,casabal,casaflux,casapool)
-    ENDIF
+  ENDIF
 
-    CALL casa_cnpbal(veg,casamet,casapool,casaflux,casabal)
+  call casa_cnpbal(veg,casamet,casapool,casaflux,casabal)
 
     CALL casa_cnpflux(casaflux,casapool,casabal,.FALSE.)
 
@@ -183,6 +184,6 @@
 
 
 
-  END SUBROUTINE biogeochem
+END SUBROUTINE biogeochem
 
-
+END MODULE biogeochem_mod
