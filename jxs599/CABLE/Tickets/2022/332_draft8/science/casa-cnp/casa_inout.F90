1e39be6bd4872b1cba6accc6bba7241ec99c304e
Jhan Srbinovsky <jxs599@nci.org.au>
2022-11-16 22:57:55 +0000

    take out readbiome NOT readphen

--- jxs599/CABLE/Tickets/2022/332_draft8/science/casa-cnp/casa_inout.F90
+++ jxs599/CABLE/Tickets/2022/332_draft8/science/casa-cnp/casa_inout.F90
@@ -32,8 +32,9 @@ MODULE casa_inout_module
 
 CONTAINS
 
-SUBROUTINE casa_readbiome(veg,soil,casabiome,casapool,casaflux,casamet,phen)
-! mst actually not used in this routine (BP sep2010)
+SUBROUTINE casa_readphen(veg,casamet,phen)
+  ! read in the tabulated modis-derived leaf phenology data
+  ! for latitude bands of 79.75 to -55.25
   USE cable_def_types_mod
   USE casadimension
   USE casaparm
@@ -41,282 +42,60 @@ SUBROUTINE casa_readbiome(veg,soil,casabiome,casapool,casaflux,casamet,phen)
   USE phenvariable
   USE cable_common_module, ONLY : knode_gl
   IMPLICIT NONE
-  TYPE (veg_parameter_type),  INTENT(INOUT) :: veg  ! vegetation parameters
-  TYPE (soil_parameter_type), INTENT(INOUT) :: soil ! soil parameters  
-  TYPE (casa_biome),          INTENT(INOUT) :: casabiome
-  TYPE (casa_pool),           INTENT(INOUT) :: casapool
-  TYPE (casa_flux),           INTENT(INOUT) :: casaflux
-  TYPE (casa_met),            INTENT(INOUT) :: casamet
-  TYPE (phen_variable),       INTENT(INOUT) :: phen
+  TYPE (veg_parameter_type), INTENT(IN)    :: veg  ! vegetation parameters
+  TYPE (casa_met),           INTENT(IN)    :: casamet
+  TYPE (phen_variable),      INTENT(INOUT) :: phen
 
   ! local variables
-  REAL(r_2), DIMENSION(mvtype)       :: slawright
-  REAL(r_2), DIMENSION(mvtype)       :: leafage,frootage,woodage
-  REAL(r_2), DIMENSION(mvtype)       :: totroot
-  REAL(r_2), DIMENSION(mvtype)       :: cwdage,metage,strage
-  REAL(r_2), DIMENSION(mvtype)       :: micage,slowage,passage,clabileage
-  REAL(r_2), DIMENSION(mvtype,mplant):: ratioCNplant
-  REAL(r_2), DIMENSION(mvtype,msoil) :: ratioCNsoil,ratioCNsoilmin,ratioCNsoilmax
-  REAL(r_2), DIMENSION(ms)           :: depthsoila,depthsoilb
-  REAL(r_2), DIMENSION(mvtype)       :: xfNminloss, xfNminleach, xnfixrate
-  REAL(r_2), DIMENSION(mvtype)       :: cleaf,cwood,cfroot,      &
-                                     cmet,cstr,ccwd,          &
-                                     cmic,cslow,cpass
-  REAL(r_2), DIMENSION(mvtype)       :: nleaf,nwood,nfroot,      &
-                                     nmet,nstr,ncwd,          &
-                                     nmic,nslow,npass,xnsoilmin
-  REAL(r_2), DIMENSION(mvtype)       :: xpleaf, xpwood, xpfroot, &
-                                     xpmet, xpstr, xpcwd,     &
-                                     xpmic,xpslow,xppass,xplab,xpsorb,xpocc
-  REAL(r_2), DIMENSION(mso)       :: xkmlabp,xpsorbmax,xfPleach
-  REAL(r_2), DIMENSION(mso,msoil) :: ratioNPsoil
-  REAL(r_2), DIMENSION(mvtype)       :: xfherbivore,xxkleafcoldmax, xxkleafdrymax
-  REAL(r_2), DIMENSION(mvtype)       :: xkuplabp
-  REAL(r_2), DIMENSION(mvtype,ms)    :: fracroot 
-  REAL(r_2) ::  xratioNPleafmin,xratioNPleafmax,         &
-                xratioNPwoodmin,xratioNPwoodmax,         &
-                xratioNPfrootmin,xratioNPfrootmax
-  INTEGER :: i,iv1,nv,ns,npt,iv,is,iso
-  INTEGER :: nv0,nv1,nv2,nv3,nv4,nv5,nv6,nv7,nv8,nv9,nv10
+  INTEGER, PARAMETER            :: nphen=8! was 10(IGBP). changed by Q.Zhang @01/12/2011
+  INTEGER np,nx,ilat
+  INTEGER, DIMENSION(271,mvtype) :: greenup, fall,  phendoy1
+  INTEGER, DIMENSION(nphen)     :: greenupx,fallx,xphendoy1
+  INTEGER, DIMENSION(nphen)     :: ivtx
+  REAL(r_2), DIMENSION(271)     :: xlat
 
-  OPEN(101,file=casafile%cnpbiome)
+  ! initilize for evergreen PFTs
+  greenup(:,:) = -50
+  fall(:,:)    = 367
+  phendoy1(:,:)= 2
 
-  if (knode_gl==0) then    
+  OPEN(101,file=casafile%phen)
+
+  if (knode_gl==0) then
     print *, '  '; print *, 'CASA_log:'
     print *, '  Opened file - '
-    print *, '  ', trim(casafile%cnpbiome)
-    print *, '  for reading cnpbiome vars.'
+    print *, '  ', trim(casafile%phen)
+    print *, '  for reading phen vars.'
     print *, 'End CASA_log:'; print *, '  '
   endif
 
-  DO i=1,3
-    READ(101,*) 
-  ENDDO
-  
-  DO nv=1,mvtype
-    READ(101,*) nv0,casabiome%ivt2(nv)
-  ENDDO
-
   READ(101,*)
-  READ(101,*)
-  DO nv=1,mvtype
-    READ(101,*) nv1,casabiome%kroot(nv),casabiome%rootdepth(nv),      &
-                casabiome%kuptake(nv),casabiome%krootlen(nv),         &
-                casabiome%kminN(nv), casabiome%kuplabP(nv),           &
-                xfherbivore(nv),leafage(nv),woodage(nv),frootage(nv), &
-                metage(nv),strage(nv),cwdage(nv),  &
-                micage(nv),slowage(nv),passage(nv),clabileage(nv),slawright(nv) 
-  ENDDO  
-
-  READ(101,*)
-  READ(101,*)
-  DO nv=1,mvtype
-    READ(101,*) nv2, &
-                casabiome%fracnpptoP(nv,leaf),casabiome%fracnpptoP(nv,wood), &
-                casabiome%fracnpptoP(nv,froot),casabiome%rmplant(nv,leaf),   &
-                casabiome%rmplant(nv,wood),casabiome%rmplant(nv,froot)
-  ENDDO 
-
-  READ(101,*)
-  READ(101,*)
-  DO nv=1,mvtype
-    READ(101,*) nv2, ratioCNplant(nv,leaf),ratioCNplant(nv,wood),   &
-         ratioCNplant(nv,froot),                                         &
-         casabiome%ftransNPtoL(nv,leaf), casabiome%ftransNPtoL(nv,wood), &
-         casabiome%ftransNPtoL(nv,froot),                                & 
-         casabiome%fracligninplant(nv,leaf),                             &
-         casabiome%fracligninplant(nv,wood),                             &
-         casabiome%fracligninplant(nv,froot),                            &
-         ratioCNsoil(nv,mic),ratioCNsoil(nv,slow),ratioCNsoil(nv,pass),  &
-         ratioCNsoilmin(nv,mic),ratioCNsoilmin(nv,slow),ratioCNsoilmin(nv,pass),  &
-         ratioCNsoilmax(nv,mic),ratioCNsoilmax(nv,slow),ratioCNsoilmax(nv,pass),  &
-         casabiome%glaimax(nv),casabiome%glaimin(nv)
+  READ(101,*) (ivtx(nx),nx=1,nphen) ! fixed at 10, as only 10 of 17 IGBP PFT
+                                    ! have seasonal leaf phenology
+  DO ilat=271,1,-1
+    READ(101,*) xlat(ilat),(greenupx(nx),nx=1,nphen), &
+                (fallx(nx),nx=1,nphen),(xphendoy1(nx),nx=1,nphen)
+    DO nx=1,nphen
+      greenup(ilat,ivtx(nx)) = greenupx(nx)
+      fall(ilat,ivtx(nx))    = fallx(nx)
+      phendoy1(ilat,ivtx(nx))= xphendoy1(nx)
+    ENDDO
   ENDDO
 
-  READ(101,*)
-  READ(101,*)
-  DO nv=1,mvtype
-    READ(101,*) nv3, cleaf(nv),cwood(nv),cfroot(nv),cmet(nv),   &
-                cstr(nv),ccwd(nv), cmic(nv), cslow(nv),cpass(nv)
-  ENDDO
-
-  READ(101,*)
-  READ(101,*)
-  DO nv=1,mvtype
-    READ(101,*) nv4, &
-         phen%TKshed(nv),xxkleafcoldmax(nv),casabiome%xkleafcoldexp(nv), &
-         xxkleafdrymax(nv),casabiome%xkleafdryexp(nv)
-  ENDDO
-
-  READ(101,*)
-  READ(101,*)
-  DO nv=1,mvtype
-    READ(101,*) nv6, &
-      casabiome%ratioNCplantmin(nv,leaf),casabiome%ratioNCplantmax(nv,leaf), &
-      casabiome%ratioNCplantmin(nv,wood),casabiome%ratioNCplantmax(nv,wood), &
-      casabiome%ratioNCplantmin(nv,froot),casabiome%ratioNCplantmax(nv,froot), &
-      xfNminloss(nv), xfNminleach(nv),xnfixrate(nv)
-  ENDDO
-
-  READ(101,*)
-  READ(101,*)
-  DO nv=1,mvtype
-    READ(101,*) nv7,nleaf(nv),nwood(nv),nfroot(nv), &
-                nmet(nv),nstr(nv), ncwd(nv), &
-                nmic(nv),nslow(nv),npass(nv),xnsoilmin(nv)
-  ENDDO
-
-  READ(101,*)
-  READ(101,*)
-  DO nv=1,mvtype
-    READ(101,*) nv8,xratioNPleafmin,xratioNPleafmax,      &
-         xratioNPwoodmin,xratioNPwoodmax,                      &
-         xratioNPfrootmin,xratioNPfrootmax,                    &
-         casabiome%ftransPPtoL(nv,leaf), casabiome%ftransPPtoL(nv,wood), &
-         casabiome%ftransPPtoL(nv,froot)
-    casabiome%ratioPcplantmin(nv,leaf)  = 1.0/(xratioNPleafmax*ratioCNplant(nv,leaf))
-    casabiome%ratioPcplantmax(nv,leaf)  = 1.0/(xratioNPleafmin*ratioCNplant(nv,leaf))
-    casabiome%ratioPcplantmin(nv,wood)  = 1.0/(xratioNPwoodmax*ratioCNplant(nv,wood))
-    casabiome%ratioPcplantmax(nv,wood)  = 1.0/(xratioNPwoodmin*ratioCNplant(nv,wood))
-    casabiome%ratioPcplantmin(nv,froot) = 1.0/(xratioNPfrootmax*ratioCNplant(nv,froot))
-    casabiome%ratioPcplantmax(nv,froot) = 1.0/(xratioNPfrootmin*ratioCNplant(nv,froot))
-  ENDDO
-
-  READ(101,*)
-  READ(101,*)
-  DO iso=1,mso
-    READ(101,*) nv9,xkmlabp(iso),xpsorbmax(iso),xfPleach(iso), &
-                ratioNPsoil(iso,mic),ratioNPsoil(iso,slow),ratioNPsoil(iso,pass)
-  ENDDO
-
-  READ(101,*)
-  READ(101,*)
-  DO nv=1,mvtype
-    READ(101,*) nv10, &
-         xpleaf(nv),xpwood(nv),xpfroot(nv),xpmet(nv),xpstr(nv),xpcwd(nv), &
-         xpmic(nv),xpslow(nv),xppass(nv),xplab(nv),xpsorb(nv),xpocc(nv)
-  ENDDO
-  CLOSE(101)
-
-  fracroot   = 0.0
-  depthsoila = 0.0
-  depthsoilb = 0.0
-  DO ns=1,ms
-    depthsoilb(ns) = depthsoilb(ns) + soil%zse(ns)
-    IF (ns==1) THEN
-      depthsoila(ns) = 0.0
-    ELSE
-      depthsoila(ns) = depthsoilb(ns-1)
-    ENDIF        
-  ENDDO
-
-  DO nv=1,mvtype
-! use the value from Wright et al. (2004) (read in) instead of equation 
-    casabiome%sla(nv)             = slawright(nv)
-    casabiome%fraclabile(nv,leaf) = deltcasa*0.6    !1/day
-    casabiome%fraclabile(nv,froot)= deltcasa*0.4    !1/day
-    casabiome%fraclabile(nv,wood) = deltcasa*0.0
-    casabiome%plantrate(nv,leaf)  = deltcasa/(leafage(nv)*(1.0-xfherbivore(nv)))
-    casabiome%plantrate(nv,froot) = deltcasa/frootage(nv)
-    casabiome%plantrate(nv,wood)  = deltcasa/woodage(nv)
-    casabiome%litterrate(nv,metb) = deltcasa/metage(nv)
-    casabiome%litterrate(nv,str)  = deltcasa/strage(nv)
-    casabiome%litterrate(nv,cwd)  = deltcasa/cwdage(nv)
-    casabiome%soilrate(nv,mic)    = deltcasa/micage(nv)
-    casabiome%soilrate(nv,slow)   = deltcasa/slowage(nv)
-    casabiome%soilrate(nv,pass)   = deltcasa/passage(nv)
-    casabiome%xkleafcoldmax(nv)   = deltcasa * xxkleafcoldmax(nv)
-    casabiome%xkleafdrymax(nv)    = deltcasa * xxkleafdrymax(nv)
-    casabiome%rmplant(nv,:)       = casabiome%rmplant(nv,:)*deltcasa 
-    casabiome%kclabrate(nv)       = deltcasa/clabileage(nv)
-  ENDDO
-
-
-  DO npt = 1, mp
-    iv1=veg%iveg(npt)
-    iso=casamet%isorder(npt)
-    ! The following to be commented out when coupled to CABLE
-    casamet%iveg2(npt) =casabiome%ivt2(iv1)
-    casamet%lnonwood(npt) = 1
-    casapool%cplant(npt,wood)  = 0.0
-    casapool%clitter(npt,cwd)  = 0.0
-    casapool%nplant(npt,wood)  = 0.0
-    casapool%nlitter(npt,cwd)  = 0.0
-    casapool%pplant(npt,wood)  = 0.0
-    casapool%plitter(npt,cwd)  = 0.0
-    IF (casamet%iveg2(npt)==forest.or.casamet%iveg2(npt)==shrub) THEN 
-      casamet%lnonwood(npt) = 0
-      casapool%cplant(npt,wood)  = cwood(iv1) 
-      casapool%clitter(npt,cwd)  = ccwd(iv1)
-      casapool%nplant(npt,wood)  = nwood(iv1) 
-      casapool%nlitter(npt,cwd)  = ncwd(iv1)
-      casapool%pplant(npt,wood)  = xpwood(iv1)
-      casapool%plitter(npt,cwd)  = xpcwd(iv1)
-    ENDIF 
-    casapool%cplant(npt,leaf)     = cleaf(iv1)
-    casapool%cplant(npt,froot)    = cfroot(iv1)
-    casapool%clabile(npt)         = 0.0
-    casapool%clitter(npt,metb)     = cmet(iv1)
-    casapool%clitter(npt,str)     = cstr(iv1)
-    casapool%csoil(npt,mic)       = cmic(iv1)
-    casapool%csoil(npt,slow)      = cslow(iv1)
-    casapool%csoil(npt,pass)      = cpass(iv1)
-    IF (icycle==1) THEN
-      casapool%rationcplant(npt,:)  = 1.0/ratiocnplant(iv1,:)
-    ENDIF 
-
-    ! initializing glai in case not reading pool file (eg. during spin)
-    casamet%glai(npt) = MAX(casabiome%glaimin(iv1), &
-                        casabiome%sla(iv1) * casapool%cplant(npt,leaf))
-
-    casaflux%fNminloss(npt)   = xfNminloss(iv1) 
-    ! comment out by ypw 12/07/2009
-    casaflux%fNminleach(npt)  = 10.0*xfNminleach(iv1) * deltcasa
-    casapool%nplant(npt,leaf) = nleaf(iv1)
-    casapool%nplant(npt,froot)= nfroot(iv1)
-    casapool%nlitter(npt,metb) = nmet(iv1)
-    casapool%nlitter(npt,str) = cstr(iv1)*ratioNCstrfix
-    casapool%nsoil(npt,mic)   = nmic(iv1)
-    casapool%nsoil(npt,slow)  = nslow(iv1)
-    casapool%nsoil(npt,pass)  = npass(iv1) 
-    casapool%nsoilmin(npt)    = xnsoilmin(iv1) 
-    casapool%pplant(npt,leaf) = xpleaf(iv1)
-    casapool%pplant(npt,froot)= xpfroot(iv1) 
-    casapool%plitter(npt,metb) = xpmet(iv1)
-    casapool%plitter(npt,str) = cstr(iv1)*ratioPCstrfix
-    casapool%psoil(npt,mic)   = xpmic(iv1)
-    casapool%psoil(npt,slow)  = xpslow(iv1)
-    casapool%psoil(npt,pass)  = xppass(iv1)
-    casapool%psoillab(npt)    = xplab(iv1)
-    casapool%psoilsorb(npt)   = xpsorb(iv1)
-    casapool%psoilocc(npt)    = xpocc(iv1)
-    casaflux%kmlabp(npt)      = xkmlabp(iso)
-    casaflux%psorbmax(npt)    = xpsorbmax(iso)
-    casaflux%fpleach(npt)     = xfPleach(iso)/(365.0)
-
-    casapool%rationcplant(npt,:)  = 1.0/ratioCNplant(iv1,:)
-    casapool%ratiopcplant(npt,:)  = casabiome%ratioPcplantmax(iv1,:)
-    casapool%rationclitter(npt,:) = casapool%nlitter(npt,:)/(casapool%clitter(npt,:)+1.0e-10)
-    casapool%ratiopclitter(npt,:) = casapool%plitter(npt,:)/(casapool%clitter(npt,:)+1.0e-10)
-    casapool%ratioNCsoil(npt,:)   = 1.0/ratioCNsoil(iv1,:)
-    casapool%ratioPCsoil(npt,:)   = 1.0/(ratioCNsoil(iv1,:)*ratioNPsoil(iso,:))
-    casapool%ratioNCsoilmin(npt,:)   = 1.0/ratioCNsoilmax(iv1,:)
-    casapool%ratioNCsoilmax(npt,:)   = 1.0/ratioCNsoilmin(iv1,:)
-    casapool%ratioNCsoilnew(npt,:)   = casapool%ratioNCsoilmax(npt,:)
+  DO np=1,mp
+    ilat=(casamet%lat(np)+55.25)/0.5+1
+    ilat= MIN(271,MAX(1,ilat))
+    phen%phase(np) = phendoy1(ilat,veg%iveg(np))
+    phen%doyphase(np,1) = greenup(ilat,veg%iveg(np)) ! DOY for greenup
+    phen%doyphase(np,2) = phen%doyphase(np,1) +14    ! DOY for steady LAI
+    phen%doyphase(np,3) = fall(ilat,veg%iveg(np))    ! DOY for leaf senescence
+    phen%doyphase(np,4) = phen%doyphase(np,3) +14    ! DOY for minimal LAI season
+    IF (phen%doyphase(np,2) > 365) phen%doyphase(np,2)=phen%doyphase(np,2)-365
+    IF (phen%doyphase(np,4) > 365) phen%doyphase(np,4)=phen%doyphase(np,4)-365
 
   ENDDO
 
-  IF (icycle==1) THEN
-    casapool%nplant(:,:)  = casapool%cplant(:,:) * casapool%rationcplant(:,:)
-  ELSE
-    casapool%Nsoil(:,:)   = casapool%ratioNCsoil(:,:) * casapool%Csoil(:,:)
-    casapool%Psoil(:,:)   = casapool%ratioPCsoil(:,:) * casapool%Csoil(:,:)
-    casapool%psoilsorb(:) = casaflux%psorbmax(:) * casapool%psoillab(:) &
-                            /(casaflux%kmlabp(:)+casapool%psoillab(:))
-  ENDIF 
-      
-
-END SUBROUTINE casa_readbiome
+END SUBROUTINE casa_readphen
 
 SUBROUTINE casa_init(casabiome,casamet,casapool,casabal,veg,phen)
 ! mst not used (BP sep2010)
