c0e777f9e0a7772ae80777216a592eb28cab3ad4
Jhan Srbinovsky <jxs599@nci.org.au>
2022-12-07 04:55:58 +0000

    i#358 flags the issue OF snow_deth_thresh

--- jxs599/CABLE/Tickets/2022/338_2/params/cable_phys_constants_mod.F90
+++ jxs599/CABLE/Tickets/2022/338_2/params/cable_phys_constants_mod.F90
@@ -1,3 +1,10 @@
+!******************************************************************************
+! This source code is part of the Community Atmosphere Biosphere Land Exchange
+! (CABLE) model. This work is licensed under the CSIRO Open Source Software
+! License Agreement (variation of the BSD / MIT License).You may not use this
+! this file except in compliance with this License. A copy of the License is
+! available at https://trac.nci.org.au/trac/cable/wiki/license.
+!******************************************************************************
 MODULE cable_phys_constants_mod
 
 IMPLICIT NONE
@@ -8,9 +15,12 @@ PUBLIC
 ! Description:
 !   CABLE physical constants
 !
-! Code Owner: Please refer to ModuleLeaders.txt
-! This file belongs in 
-!-----------------------------------------------------------------------------
+! This MODULE is USEd throughout CABLE.
+!
+! Module specific documentation:https://trac.nci.org.au/trac/cable/wiki/TBC
+! Where it fits in the model flow:https://trac.nci.org.au/trac/cable/wiki/TBC
+!******************************************************************************
+
 
   REAL, PARAMETER :: tfrz   = 273.16        ! Temp (K) corresp. to 0 C
   REAL, PARAMETER :: sboltz = 5.67e-8       ! Stefan-Boltz. const (W/m2/K4)
@@ -70,5 +80,8 @@ PUBLIC
   REAL, PARAMETER :: zetpos = 1.0     ! positive limit on za/L when niter>=3
   REAL, PARAMETER :: zdlin  = 1.0     ! height frac of d below which TL linear
   REAL, PARAMETER :: umin   = 0.1     ! guarantees convergence, was 0.01 
+
+!model parameter shared across subroutines -> cable_phys_constants
+REAL, PARAMETER :: snow_depth_thresh = 1.0
 
 END MODULE cable_phys_constants_mod
