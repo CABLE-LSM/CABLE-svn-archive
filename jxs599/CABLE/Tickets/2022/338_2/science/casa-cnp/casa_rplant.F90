6b91508c35dbfcdd2a7c1efaece39552161cb430
Jhan Srbinovsky <jxs599@nci.org.au>
2022-12-12 05:27:17 +0000

    are these simply the mapped versions of science subrs

--- jxs599/CABLE/Tickets/2022/338_2/science/casa-cnp/casa_rplant.F90
+++ jxs599/CABLE/Tickets/2022/338_2/science/casa-cnp/casa_rplant.F90
@@ -1,4 +1,4 @@
-#define UM_BUILD YES
+!#define UM_BUILD YES
 !==============================================================================
 ! This source code is part of the
 ! Australian Community Atmosphere Biosphere Land Exchange (CABLE) model.
