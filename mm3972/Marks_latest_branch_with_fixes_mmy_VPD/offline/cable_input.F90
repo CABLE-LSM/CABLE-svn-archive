f2dc2bedc9cc916870dd105f1fcbd64e20652a31
Mengyuan Mu <mm3972@nci.org.au>
2023-03-19 23:19:16 +0000

    comment out qv*0.9

--- mm3972/Marks_latest_branch_with_fixes_mmy_VPD/offline/cable_input.F90
+++ mm3972/Marks_latest_branch_with_fixes_mmy_VPD/offline/cable_input.F90
@@ -2917,17 +2917,17 @@ SUBROUTINE get_met_data(spinup,spinConv,met,soil,rad,                          &
     met%tvair = met%tk
     met%tvrad = met%tk
 
-    ! __________________ MMY@Dec2022 _________________
-    met%qv = met%qv*0.9 ! MMY@Nov2022 reduce air humidity to reduce VPD
-
-    WHERE (met%qv<ranges%Qair(1)) 
-       met%qv=ranges%Qair(1)
-    END WHERE
-
-    WHERE (met%qv>ranges%Qair(2)) 
-       met%qv=ranges%Qair(2)
-    END WHERE
-    ! ________________________________________________
+    !! __________________ MMY@Dec2022 _________________
+    !met%qv = met%qv*0.9 ! MMY@Nov2022 reduce air humidity to reduce VPD
+    !
+    !WHERE (met%qv<ranges%Qair(1)) 
+    !   met%qv=ranges%Qair(1)
+    !END WHERE
+    !
+    !WHERE (met%qv>ranges%Qair(2)) 
+    !   met%qv=ranges%Qair(2)
+    !END WHERE
+    !! ________________________________________________
 
    IF(check%ranges) THEN 
        ! Check ranges are okay:
